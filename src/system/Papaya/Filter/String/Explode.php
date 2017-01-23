49b5bfa5e8d27efab3ebc62f9342bf64b03bea10
Thomas Weinert <thomas@weinert.info>
2017-01-23 16:15:52 +0100

    Added: filter that explodes a string into tokens

---
+++ src/system/Papaya/Filter/String/Explode.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Papaya filter class for an string consisting of several parts
+ *
+ * @copyright 2017 by papaya Software GmbH - All rights reserved.
+ * @link http://www.papaya-cms.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ * You can redistribute and/or modify this script under the terms of the GNU General Public
+ * License (GPL) version 2, provided that the copyright and license notes, including these
+ * lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ *
+ * @package Papaya-Library
+ * @subpackage Filter
+ */
+
+/**
+ * Papaya filter class for an string consisting of several parts
+ *
+ * It will explode the string by the provided separator and filter each part using
+ * the provided element filter.
+ *
+ * @package Papaya-Library
+ * @subpackage Filter
+ */
+
+class PapayaFilterStringExplode implements PapayaFilter {
+
+  const TRIM_TOKENS = 1;
+
+  /**
+   * @var string
+   */
+  private $_separator;
+
+  /**
+   * @var PapayaFilter
+   */
+  private $_filter;
+
+  /**
+   * @var int
+   */
+  private $_options;
+
+  public function __construct(
+    $separator = ',', PapayaFilter $elementFilter = NULL, $options = self::TRIM_TOKENS
+  ) {
+    $this->_separator = $separator;
+    $this->_filter = $elementFilter;
+    $this->_options = $options;
+  }
+
+  /**
+   * @param mixed $value
+   * @return bool
+   * @throws PapayaFilterExceptionEmpty
+   */
+  public function validate($value) {
+    if (empty($value)) {
+      throw new PapayaFilterExceptionEmpty();
+    }
+    $tokens = explode($this->_separator, (string)$value);
+    if ($this->_filter instanceof PapayaFilter) {
+      foreach ($tokens as $token) {
+        if (PapayaUtilBitwise::inBitmask(self::TRIM_TOKENS, $this->_options)) {
+          $token = trim($token);
+        }
+        $this->_filter->validate($token);
+      }
+    }
+    return TRUE;
+  }
+
+  /**
+   * @param mixed|NULL $value
+   * @return array|null
+   */
+  public function filter($value) {
+    $tokens = explode($this->_separator, (string)$value);
+    $result = [];
+    foreach ($tokens as $token) {
+      if (PapayaUtilBitwise::inBitmask(self::TRIM_TOKENS, $this->_options)) {
+        $token = trim($token);
+      }
+      if ($this->_filter instanceof PapayaFilter) {
+        $filteredToken =  $this->_filter->filter($token);
+      } else {
+        $filteredToken = empty($token) ? NULL : $token;
+      }
+      if (NULL !== $filteredToken) {
+        $result[] = $filteredToken;
+      }
+    }
+    return empty($result) ? NULL : $result;
+  }
+
+}
--- tests/system/Papaya/Ui/Dialog/Field/Date/RangeTest.php
+++ tests/system/Papaya/Ui/Dialog/Field/Date/RangeTest.php
@@ -6,9 +6,11 @@ class PapayaUiDialogFieldDateRangeTest extends PapayaTestCase {
   public function testAppendTo() {
     $field = new PapayaUiDialogFieldDateRange('Caption', 'name');
     $this->assertXmlStringEqualsXmlString(
-      '<field caption="Caption" class="DialogFieldDateRange" data-include-time="true" error="no">'.
-        '<input name="name[start]" type="date"/>'.
-        '<input name="name[end]" type="date"/>'.
+      '<field caption="Caption" class="DialogFieldDateRange" data-include-time="false" error="no">'.
+        '<group>'.
+          '<input name="name[start]" type="date"/>'.
+          '<input name="name[end]" type="date"/>'.
+        '</group>'.
       '</field>',
       $field->getXml()
     );
