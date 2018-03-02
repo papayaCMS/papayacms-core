505951baf11ff0ec8884937b8b5da58bb324e8d2
Thomas Weinert <thomas@weinert.info>
2018-03-02 12:10:14 +0100

    Remove date from uploaded file properties (filectime is not a useful value)

--- src/system/Papaya/Media/File/Info/Basic.php
+++ src/system/Papaya/Media/File/Info/Basic.php
@@ -4,8 +4,7 @@ class PapayaMediaFileInfoBasic extends PapayaMediaFileInfo {
   protected function fetchProperties() {
     $fileName = $this->getFile();
     return array(
-      'size' => filesize($fileName),
-      'file_created' => filectime($fileName)
+      'size' => filesize($fileName)
     );
   }
 
