b3fee55337f51f32403b83b37801f343942dd96c
Thomas Weinert <thomas@weinert.info>
2018-04-26 15:16:22 +0200

    Fix annotations, cleanup inspection messages

--- src/system/Papaya/Content/Theme/Set.php
+++ src/system/Papaya/Content/Theme/Set.php
@@ -1,32 +1,33 @@
 <?php
 /**
-* Load/save a the set main record (contains name and id)
-*
-* @copyright 2012 by papaya Software GmbH - All rights reserved.
-* @link http://www.papaya-cms.com/
-* @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
-*
-* You can redistribute and/or modify this script under the terms of the GNU General Public
-* License (GPL) version 2, provided that the copyright and license notes, including these
-* lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-* FOR A PARTICULAR PURPOSE.
-*
-* @package Papaya-Library
-* @subpackage Content
-* @version $Id: Set.php 39403 2014-02-27 14:25:16Z weinert $
-*/
+ * papaya CMS
+ *
+ * @copyright 2000-2018 by papayaCMS project - All rights reserved.
+ * @link http://www.papaya-cms.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ *  You can redistribute and/or modify this script under the terms of the GNU General Public
+ *  License (GPL) version 2, provided that the copyright and license notes, including these
+ *  lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.
+ */
 
 /**
-* Load/save a the set main record (contains name and id)
-*
-* @package Papaya-Library
-* @subpackage Content
-*/
+ * Load/save a the theme set main record (contains name and id)
+ *
+ * @package Papaya-Library
+ * @subpackage Content
+ *
+ * @property string $id
+ * @property string $title
+ * @property string $theme
+ * @property array $values
+ */
 class PapayaContentThemeSet extends PapayaDatabaseRecord {
 
   /**
-   * Map field names to more convinient property names
+   * Map field names to more convenient property names
    *
    * @var array(string=>string)
    */
@@ -61,32 +62,39 @@ class PapayaContentThemeSet extends PapayaDatabaseRecord {
   }
 
   /**
-  * Link options need to be deserialized.
-  *
-  * @param string $context
-  * @param string $property
-  * @param string $field
-  * @param mixed $value
-  * @return mixed
-  */
-  public function mapFieldToProperty($context, $property, $field, $value) {
-    if ($property == 'values') {
+   * Link options need to be deserialized.
+   *
+   * @param string $context
+   * @param string $property
+   * @param string $field
+   * @param mixed $value
+   * @return mixed
+   *
+   */
+  public function mapFieldToProperty(
+    /** @noinspection PhpUnusedParameterInspection */
+    $context, $property, $field, $value
+  ) {
+    if ('values' === $property) {
       return PapayaUtilStringXml::unserializeArray((string)$value);
     }
     return $value;
   }
 
   /**
-  * Link options need to be serialized.
-  *
-  * @param string $context
-  * @param string $property
-  * @param string $field
-  * @param mixed $value
-  * @return mixed
-  */
-  public function mapPropertyToField($context, $property, $field, $value) {
-    if ($property == 'values') {
+   * Link options need to be serialized.
+   *
+   * @param string $context
+   * @param string $property
+   * @param string $field
+   * @param mixed $value
+   * @return mixed
+   */
+  public function mapPropertyToField(
+    /** @noinspection PhpUnusedParameterInspection */
+    $context, $property, $field, $value
+  ) {
+    if ('values' === $property) {
       return PapayaUtilStringXml::serializeArray((array)$value);
     }
     return $value;
