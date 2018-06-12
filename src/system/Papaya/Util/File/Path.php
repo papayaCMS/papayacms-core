ef6fb2a1f9ade21796190d78148da12ae8de8e8f
Thomas Weinert <thomas@weinert.info>
2018-06-12 14:21:28 +0200

    Fix trailing slashes for module paths

--- src/system/Papaya/Util/File/Path.php
+++ src/system/Papaya/Util/File/Path.php
@@ -1,21 +1,17 @@
 <?php
 /**
-* Papaya Utilities class with static function for paths
-*
-* @copyright 2009 by papaya Software GmbH - All rights reserved.
-* @link http://www.papaya-cms.com/
-* @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
-*
-* You can redistribute and/or modify this script under the terms of the GNU General Public
-* License (GPL) version 2, provided that the copyright and license notes, including these
-* lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-* FOR A PARTICULAR PURPOSE.
-*
-* @package Papaya-Library
-* @subpackage Util
-* @version $Id: Path.php 39429 2014-02-27 20:14:26Z weinert $
-*/
+ * papaya CMS
+ *
+ * @copyright 2000-2018 by papayaCMS project - All rights reserved.
+ * @link http://www.papaya-cms.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ *  You can redistribute and/or modify this script under the terms of the GNU General Public
+ *  License (GPL) version 2, provided that the copyright and license notes, including these
+ *  lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.
+ */
 
 /**
 * Papaya Utilities class with static function for paths
@@ -155,11 +151,11 @@ class PapayaUtilFilePath {
    * @return string
    */
   public static function getVendorPath() {
-    $root = PapayaUtilFilePath::getDocumentRoot();
+    $root = self::getDocumentRoot();
     if (is_dir($root.'/../vendor')) {
-      return '../vendor';
+      return '../vendor/';
     }
-    return 'vendor';
+    return 'vendor/';
   }
 
   /**
@@ -167,11 +163,11 @@ class PapayaUtilFilePath {
    * @return string
    */
   public static function getSourcePath() {
-    $root = PapayaUtilFilePath::getDocumentRoot();
+    $root = self::getDocumentRoot();
     if (is_dir($root.'/../src')) {
-      return '../src';
+      return '../src/';
     }
-    return 'src';
+    return 'src/';
   }
 
   /**
