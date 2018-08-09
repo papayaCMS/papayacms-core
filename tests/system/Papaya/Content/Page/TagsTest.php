4e06d04dd311438290b1766b17b64041c4104f72
Thomas Weinert <thomas@weinert.info>
2018-08-09 16:45:37 +0200

    Add: PapayaContentPageTags should provide category data and filter

--- tests/system/Papaya/Content/Page/TagsTest.php
+++ tests/system/Papaya/Content/Page/TagsTest.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * papaya CMS
+ *
+ * @copyright 2000-2018 by papayaCMS project - All rights reserved.
+ * @link http://www.papaya-cms.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ *  You can redistribute and/or modify this script under the terms of the GNU General Public
+ *  License (GPL) version 2, provided that the copyright and license notes, including these
+ *  lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.
+ */
+
 require_once __DIR__.'/../../../../bootstrap.php';
 
 class PapayaContentPageTagsTest extends PapayaTestCase {
@@ -37,7 +51,10 @@ class PapayaContentPageTagsTest extends PapayaTestCase {
     $databaseAccess
       ->expects($this->once())
       ->method('queryFmt')
-      ->with($this->isType('string'), array('table_tag_links', 'table_tag_trans', 0, 'topic', 23))
+      ->with(
+        $this->isType('string'),
+        array('table_tag_links', 'table_tag_trans', 0, 'table_tag', 'table_tag_category', 'topic', 23)
+      )
       ->will($this->returnValue($databaseResult));
     $tags = new PapayaContentPageTags();
     $tags->setDatabaseAccess($databaseAccess);
@@ -99,7 +116,10 @@ class PapayaContentPageTagsTest extends PapayaTestCase {
     $databaseAccess
       ->expects($this->once())
       ->method('queryFmt')
-      ->with($this->isType('string'), array('table_tag_links', 'table_tag_trans', 2, 'topic', 23))
+      ->with(
+        $this->isType('string'),
+        array('table_tag_links', 'table_tag_trans', 2, 'table_tag', 'table_tag_category', 'topic', 23)
+      )
       ->will($this->returnValue($databaseResult));
     $tags = new PapayaContentPageTags();
     $tags->setDatabaseAccess($databaseAccess);
