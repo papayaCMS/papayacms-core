67016644912de0884baf9ba3899c005c4f60aa96
Thomas Weinert <thomas@weinert.info>
2018-08-17 22:03:41 +0200

    Refactor Papaya* classes to namespace Papaya\*

--- tests/system/Papaya/Ui/Dialog/Field/XHTMLTest.php
+++ tests/system/Papaya/Ui/Dialog/Field/XHTMLTest.php
@@ -16,7 +16,7 @@
 namespace Papaya\UI\Dialog\Field;
 require_once __DIR__.'/../../../../../bootstrap.php';
 
-class XhtmlTest extends \Papaya\TestCase {
+class XHTMLTest extends \Papaya\TestCase {
 
   /**
    * @covers \Papaya\UI\Dialog\Field\XHTML::__construct
