ae8ea3ef4d6d60f0cf994c9b4454b4992403495e
Thomas Weinert <thomas@weinert.info>
2018-09-18 15:11:09 +0200

    Refactor, make use of parent implementation

--- src/system/Papaya/Administration/Theme/Editor/Changes.php
+++ src/system/Papaya/Administration/Theme/Editor/Changes.php
@@ -40,42 +40,21 @@ class Changes extends \Papaya\Administration\Page\Part {
   private $_themeHandler;
 
   /**
-   * Append changes commands to parent xml element
-   *
-   * @param XML\Element $parent
-   */
-  public function appendTo(XML\Element $parent) {
-    $parent->append($this->commands());
-  }
-
-  /**
    * Commands, actual actions
    *
-   * @param UI\Control\Command\Controller|UI\Control\Command $commands
-   * @return UI\Control\Command\Controller|UI\Control\Command
+   * @param string $name
+   * @param string $default
+   * @return UI\Control\Command\Controller
    */
-  public function commands(UI\Control\Command $commands = NULL) {
-    if (NULL !== $commands) {
-      $this->_commands = $commands;
-    } elseif (NULL === $this->_commands) {
-      $this->_commands = new UI\Control\Command\Controller('cmd');
-      $this->_commands->owner($this);
-      $this->_commands['skin_edit'] =
-      $command = new Changes\Skin\Change($this->themeSet());
-      $this->_commands['skin_delete'] =
-      $command = new Changes\Skin\Remove($this->themeSet());
-      $this->_commands['skin_import'] =
-      $command = new Changes\Skin\Import(
-        $this->themeSet(), $this->themeHandler()
-      );
-      $this->_commands['skin_export'] =
-      $command = new Changes\Skin\Export(
-        $this->themeSet(), $this->themeHandler()
-      );
-      $this->_commands['values_edit'] =
-      $command = new Changes\Dialog($this->themeSet());
-    }
-    return $this->_commands;
+  protected function _createCommands($name = 'cmd', $default = 'skin_edit') {
+    $commands = new UI\Control\Command\Controller('cmd');
+    $commands->owner($this);
+    $commands['skin_edit'] = new Changes\Skin\Change($this->themeSet());
+    $commands['skin_delete'] = new Changes\Skin\Remove($this->themeSet());
+    $commands['skin_import'] = new Changes\Skin\Import($this->themeSet(), $this->themeHandler());
+    $commands['skin_export'] = new Changes\Skin\Export($this->themeSet(), $this->themeHandler());
+    $commands['values_edit'] = new Changes\Dialog($this->themeSet());
+    return $commands;
   }
 
   /**
