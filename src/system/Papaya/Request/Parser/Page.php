eb275138a7e66e5d9300fb4507982d3974b853e2
Thomas Weinert <thomas@weinert.info>
2018-09-24 16:05:35 +0200

    Cleanup/Fix formatting

--- src/system/Papaya/Request/Parser/Page.php
+++ src/system/Papaya/Request/Parser/Page.php
@@ -22,10 +22,10 @@ namespace Papaya\Request\Parser;
  */
 class Page extends \Papaya\Request\Parser {
   /**
-  * PCRE pattern for page links
-  *
-  * @var string
-  */
+   * PCRE pattern for page links
+   *
+   * @var string
+   */
   private $_pattern = '(
     ^(/[^/\n?#]+)*/ # anything before the last path /
     (?:(?P<page_title>(?:%[\\dA-Fa-f]{2}|[\\sa-zA-Z\\d_-]+)+)\\.) # title
