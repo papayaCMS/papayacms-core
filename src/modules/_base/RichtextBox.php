19979b9923b86c27ef2dd3ac690421bf6531326e
Thomas Weinert <thomas@weinert.info>
2019-10-02 16:10:26 +0200

    [FEATURE] Add Richtext Box Module

---
+++ src/modules/_base/RichtextBox.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * papaya CMS
+ *
+ * @copyright 2000-2019 by papayaCMS project - All rights reserved.
+ * @link http://www.papaya-cms.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ *  You can redistribute and/or modify this script under the terms of the GNU General Public
+ *  License (GPL) version 2, provided that the copyright and license notes, including these
+ *  lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.
+ */
+
+namespace Papaya\Modules\Core {
+
+  use Papaya\Administration\Plugin\Editor\Dialog as EditorDialog;
+  use Papaya\Application\Access as ApplicationAccess;
+  use Papaya\Plugin\Quoteable as QuotablePlugin;
+  use Papaya\Plugin\Editable as EditablePlugin;
+  use Papaya\Plugin\Editable\Content as EditableContent;
+  use Papaya\Plugin\Editor as PluginEditor;
+  use Papaya\UI\Dialog\Field as DialogField;
+  use Papaya\UI\Dialog\Options as DialogOptions;
+  use Papaya\UI\Text\Translated as TranslatedText;
+  use Papaya\XML\Element as XMLElement;
+
+  class RichtextBox implements ApplicationAccess, QuotablePlugin, EditablePlugin {
+
+    use EditableContent\Aggregation;
+
+    const FIELD_TEXT = 'text';
+
+    const _DEFAULTS = [
+      self::FIELD_TEXT => ''
+    ];
+
+    /**
+     * @param EditableContent $content
+     *
+     * @return PluginEditor
+     */
+    public function createEditor(EditableContent $content) {
+      $editor = new EditorDialog($content);
+      $editor->papaya($this->papaya());
+      $dialog = $editor->dialog();
+      $dialog->options->captionStyle = DialogOptions::CAPTION_NONE;
+      $dialog->fields[] = new DialogField\Textarea\Richtext(
+        new TranslatedText('Text'),
+        self::FIELD_TEXT,
+        20,
+        self::_DEFAULTS[self::FIELD_TEXT],
+        NULL,
+        DialogField\Textarea\Richtext::RTE_SIMPLE
+      );
+      return $editor;
+    }
+
+    /**
+     * @param XMLElement $parent
+     */
+    public function appendQuoteTo(XMLElement $parent) {
+      $content = $this->content()->withDefaults(self::_DEFAULTS);
+      $parent->appendElement('text')->appendXML($content[self::FIELD_TEXT]);
+    }
+  }
+}
+
