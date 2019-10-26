bdba633b353dad2b50e9900a2a7e76f41e465d17
Thomas Weinert <thomas@weinert.info>
2019-10-26 17:13:35 +0200

    [TEST] Add Test For Papaya\Router\Route\Error

---
+++ tests/system/Papaya/Router/Route/ErrorTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * papaya CMS
+ *
+ * @copyright 2000-2019 by papayaCMS project - All rights reserved.
+ * @link http://www.papaya-cms.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ *  You can redistribute and/or modify this script under the terms of the GNU General Public
+ *  License (GPL) version 2, provided that the copyright and license notes, including these
+ *  lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.
+ */
+
+namespace Papaya\Router\Route {
+
+  use Papaya\Response\Failure as FailureResponse;
+  use Papaya\Router;
+  use Papaya\TestCase;
+
+  /**
+   * @covers \Papaya\Router\Route\Error
+   */
+  class ErrorTest extends TestCase {
+
+    public function testRouteReturnErrorResponse() {
+      $router = $this->createMock(Router::class);
+      $route = new Error('message', 500, 'error.identifier');
+
+      $response = $route($router);
+      $this->assertInstanceOf(FailureResponse::class, $response);
+    }
+  }
+
+}
