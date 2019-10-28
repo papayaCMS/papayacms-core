e3b47994323aa58c470d0668285300b24f89529a
Thomas Weinert <thomas@weinert.info>
2019-10-28 15:50:40 +0100

    [TEST] Add Tests For UI\Control\Command\Condition\Value

---
+++ tests/system/Papaya/UI/Control/Command/Condition/ValueTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * papaya CMS
+ *
+ * @copyright 2000-2019 by papayaCMS project - All rights reserved.
+ * @link http://www.papaya-cms.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ *  You can redistribute and/or modify this script under the terms of the GNU General Public
+ *  License (GPL) version 2, provided that the copyright and license notes, including these
+ *  lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.
+ */
+
+namespace Papaya\UI\Control\Command\Condition {
+
+  /**
+   * @covers \Papaya\UI\Control\Command\Condition\Value
+   */
+  class ValueTest extends \PHPUnit_Framework_TestCase {
+
+    public function testValidateExpectingTrue() {
+      $condition = new Value(TRUE);
+      $this->assertTrue($condition->validate());
+    }
+
+    public function testValidateExpectingFalse() {
+      $condition = new Value(FALSE);
+      $this->assertFalse($condition->validate());
+    }
+  }
+
+}
