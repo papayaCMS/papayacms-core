258de15ed70a467e3aed7449ff16a3c043b0fe1d
Thomas Weinert <thomas@weinert.info>
2019-10-28 11:06:09 +0100

    [TEST] Add Test For Text\Transliteration\ASCII\Mapping

--- tests/system/Papaya/Text/Transliteration/ASCII/MappingTest.php
+++ tests/system/Papaya/Text/Transliteration/ASCII/MappingTest.php
@@ -13,143 +13,118 @@
  *  FOR A PARTICULAR PURPOSE.
  */
 
-namespace Papaya\Text\Transliteration\ASCII;
-require_once __DIR__.'/../../../../../bootstrap.php';
+namespace Papaya\Text\Transliteration\ASCII {
 
-class MappingTest extends \Papaya\TestCase {
+  use Papaya\TestCase;
 
-  /**
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::__construct
-   */
-  public function testConstructor() {
-    $mapping = new Mapping();
-    $this->assertFileExists($mapping->getFile(0, 'generic'));
-  }
+  require_once __DIR__.'/../../../../../bootstrap.php';
 
   /**
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::get
-   * @dataProvider getMappedCharacterDataProvider
-   * @param string $expected
-   * @param string $codePoint
-   * @param string $language
+   * @covers \Papaya\Text\Transliteration\ASCII\Mapping
    */
-  public function testGet($expected, $codePoint, $language) {
-    $mapping = new Mapping();
-    $actual = $mapping->get($codePoint, $language);
-    $this->assertSame(
-      $expected, $actual
-    );
-  }
+  class MappingTest extends TestCase {
 
-  /**
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::clear
-   */
-  public function testClear() {
-    $mapping = new Mapping();
-    $mapping->lazyLoad(0, 'de');
-    $mapping->clear();
-    $this->assertFalse($mapping->isLoaded(0, 'de'));
-  }
+    public function testConstructor() {
+      $mapping = new Mapping();
+      $this->assertFileExists($mapping->getFile(0, 'generic'));
+    }
 
-  /**
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::isLoaded
-   */
-  public function testIsLoadedBeforeLoading() {
-    $mapping = new Mapping();
-    $this->assertFalse($mapping->isLoaded(0, 'de'));
-  }
+    /**
+     * @dataProvider getMappedCharacterDataProvider
+     * @param string $expected
+     * @param string $codePoint
+     * @param string $language
+     */
+    public function testGet($expected, $codePoint, $language) {
+      $mapping = new Mapping();
+      $actual = $mapping->get($codePoint, $language);
+      $this->assertSame(
+        $expected, $actual
+      );
+    }
 
-  /**
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::isLoaded
-   */
-  public function testIsLoadedAfterLoading() {
-    $mapping = new Mapping();
-    $mapping->lazyLoad(0, 'de');
-    $this->assertTrue($mapping->isLoaded(0, 'de'));
-  }
+    public function testClear() {
+      $mapping = new Mapping();
+      $mapping->lazyLoad(0, 'de');
+      $mapping->clear();
+      $this->assertFalse($mapping->isLoaded(0, 'de'));
+    }
 
-  /**
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::lazyLoad
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::add
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::link
-   */
-  public function testLazyLoadLanguageSpecificMapping() {
-    $mapping = new Mapping();
-    $mapping->lazyLoad(0, 'de');
-    $result = $this->readAttribute($mapping, '_mappingTables');
-    $this->assertArrayHasKey('de', $result);
-  }
+    public function testIsLoadedBeforeLoading() {
+      $mapping = new Mapping();
+      $this->assertFalse($mapping->isLoaded(0, 'de'));
+    }
 
-  /**
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::lazyLoad
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::add
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::link
-   */
-  public function testLazyLoadFallbackToGeneric() {
-    $mapping = new Mapping();
-    $mapping->lazyLoad(5, 'de');
-    $result = $this->readAttribute($mapping, '_mappingTables');
-    $this->assertArrayHasKey('de', $result);
-  }
+    public function testIsLoadedAfterLoading() {
+      $mapping = new Mapping();
+      $mapping->lazyLoad(0, 'de');
+      $this->assertTrue($mapping->isLoaded(0, 'de'));
+    }
 
-  /**
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::lazyLoad
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::add
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::link
-   */
-  public function testLazyLoadGeneric() {
-    $mapping = new Mapping();
-    $mapping->lazyLoad(5, 'generic');
-    $result = $this->readAttribute($mapping, '_mappingTables');
-    $this->assertArrayHasKey('generic', $result);
-  }
+    public function testLazyLoadLanguageSpecificMapping() {
+      $mapping = new Mapping();
+      $mapping->lazyLoad(0, 'de');
+      $result = $this->readAttribute($mapping, '_mappingTables');
+      $this->assertArrayHasKey('de', $result);
+    }
 
-  /**
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::lazyLoad
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::add
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::link
-   */
-  public function testLazyLoadMultipleCall() {
-    $mapping = new Mapping();
-    $mapping->lazyLoad(5, 'de');
-    $mapping->lazyLoad(5, 'generic');
-    $result = $this->readAttribute($mapping, '_mappingTables');
-    $this->assertEquals(
-      $result['generic'], $result['de']
-    );
-  }
+    public function testLazyLoadFallbackToMainLanguage() {
+      $mapping = new Mapping();
+      $mapping->lazyLoad(5, 'de-DE');
+      $result = $this->readAttribute($mapping, '_mappingTables');
+      $this->assertArrayHasKey('de-DE', $result);
+    }
 
-  /**
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::getFile
-   */
-  public function testGetFileGeneric() {
-    $mapping = new Mapping();
-    $this->assertStringEndsWith(
-      'utf8/external/x00.php',
-      $mapping->getFile(0, 'generic')
-    );
-  }
+    public function testLazyLoadFallbackToGeneric() {
+      $mapping = new Mapping();
+      $mapping->lazyLoad(5, 'de');
+      $result = $this->readAttribute($mapping, '_mappingTables');
+      $this->assertArrayHasKey('de', $result);
+    }
 
-  /**
-   * @covers \Papaya\Text\Transliteration\ASCII\Mapping::getFile
-   */
-  public function testGetFileGerman() {
-    $mapping = new Mapping();
-    $this->assertStringEndsWith(
-      'utf8/external/de/x00.php',
-      $mapping->getFile(0, 'de')
-    );
-  }
+    public function testLazyLoadGeneric() {
+      $mapping = new Mapping();
+      $mapping->lazyLoad(5, 'generic');
+      $result = $this->readAttribute($mapping, '_mappingTables');
+      $this->assertArrayHasKey('generic', $result);
+    }
 
-  public static function getMappedCharacterDataProvider() {
-    return array(
-      array('a', 228, 'generic'), // LATIN SMALL LETTER A WITH DIAERESIS
-      array('ae', 228, 'de'), // LATIN SMALL LETTER A WITH DIAERESIS
-      array('ae', 228, 'de-DE'), // LATIN SMALL LETTER A WITH DIAERESIS
-      array('a', 97, 'de'), // LATIN SMALL LETTER A
-      array('r', 1088, 'de'), // CYRILLIC SMALL LETTER ER
-      array('', 0, 'generic'), // NULL
-      array(NULL, 999999, 'generic'), // invalid codepoint
-    );
+    public function testLazyLoadMultipleCall() {
+      $mapping = new Mapping();
+      $mapping->lazyLoad(5, 'de');
+      $mapping->lazyLoad(5, 'generic');
+      $result = $this->readAttribute($mapping, '_mappingTables');
+      $this->assertEquals(
+        $result['generic'], $result['de']
+      );
+    }
+
+    public function testGetFileGeneric() {
+      $mapping = new Mapping();
+      $this->assertStringEndsWith(
+        'utf8/external/x00.php',
+        $mapping->getFile(0, 'generic')
+      );
+    }
+
+    public function testGetFileGerman() {
+      $mapping = new Mapping();
+      $this->assertStringEndsWith(
+        'utf8/external/de/x00.php',
+        $mapping->getFile(0, 'de')
+      );
+    }
+
+    public static function getMappedCharacterDataProvider() {
+      return [
+        ['a', 228, 'generic'], // LATIN SMALL LETTER A WITH DIAERESIS
+        ['ae', 228, 'de'], // LATIN SMALL LETTER A WITH DIAERESIS
+        ['ae', 228, 'de-DE'], // LATIN SMALL LETTER A WITH DIAERESIS
+        ['a', 97, 'de'], // LATIN SMALL LETTER A
+        ['r', 1088, 'de'], // CYRILLIC SMALL LETTER ER
+        ['', 0, 'generic'], // NULL
+        [NULL, 999999, 'generic'], // invalid codepoint
+      ];
+    }
   }
 }
