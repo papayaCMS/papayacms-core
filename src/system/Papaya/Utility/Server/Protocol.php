185195d739ab6c7ca3f39061f0db90505471400a
Thomas Weinert <thomas@weinert.info>
2019-10-30 16:32:41 +0100

    [CLEANUP] Cleanup Typos in Utility\Server\Protocol

--- src/system/Papaya/Utility/Server/Protocol.php
+++ src/system/Papaya/Utility/Server/Protocol.php
@@ -12,69 +12,71 @@
  *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  *  FOR A PARTICULAR PURPOSE.
  */
-namespace Papaya\Utility\Server;
-
-/**
- * Static utility class to check if thttp or http is used.
- *
- * @package Papaya-Library
- * @subpackage Util
- */
-class Protocol {
-  const BOTH = 0;
-
-  const HTTP = 1;
-
-  const HTTPS = 2;
+namespace Papaya\Utility\Server {
 
   /**
-   * Return TRUE if 'https' is used, FALSE if not.
+   * Static utility class to check if http or https is used.
    *
-   * If the custom header X-PAPAYA-HTTPS is provided it is compared to the constant
-   * PAPAYA_HEADER_HTTPS_TOKEN. If the header equals the token and the token is 32 bytes the
-   * request is threated as HTTPS always.
-   *
-   * @return bool
+   * @package Papaya-Library
+   * @subpackage Util
    */
-  public static function isSecure() {
-    if (isset($_SERVER['X_PAPAYA_HTTPS'])) {
-      $header = $_SERVER['X_PAPAYA_HTTPS'];
-    } elseif (isset($_SERVER['HTTP_X_PAPAYA_HTTPS'])) {
-      $header = $_SERVER['HTTP_X_PAPAYA_HTTPS'];
-    } else {
-      $header = NULL;
-    }
-    if (
-      NULL !== $header &&
-      \defined('PAPAYA_HEADER_HTTPS_TOKEN') &&
-      PAPAYA_HEADER_HTTPS_TOKEN === $header &&
-      32 === \strlen(PAPAYA_HEADER_HTTPS_TOKEN)) {
-      return TRUE;
-    }
-    return (isset($_SERVER['HTTPS']) && 'on' === \strtolower($_SERVER['HTTPS']));
-  }
+  class Protocol {
+    const BOTH = 0;
 
-  /**
-   * Return the currently used protocol. "http" or "https".
-   *
-   * @param int $mode
-   *
-   * @return string
-   */
-  public static function get($mode = self::BOTH) {
-    if (self::BOTH !== $mode) {
-      return self::HTTPS === $mode ? 'https' : 'http';
-    }
-    return self::isSecure() ? 'https' : 'http';
-  }
+    const HTTP = 1;
 
-  /**
-   * Return the default port
-   *
-   * @return string
-   */
-  public static function getDefaultPort() {
-    return (isset($_SERVER['HTTPS']) && 'on' === \strtolower($_SERVER['HTTPS']))
-      ? 443 : 80;
+    const HTTPS = 2;
+
+    /**
+     * Return TRUE if 'https' is used, FALSE if not.
+     *
+     * If the custom header X-PAPAYA-HTTPS is provided it is compared to the constant
+     * PAPAYA_HEADER_HTTPS_TOKEN. If the header equals the token and the token is 32 bytes the
+     * request is threat as HTTPS always.
+     *
+     * @return bool
+     */
+    public static function isSecure() {
+      if (isset($_SERVER['X_PAPAYA_HTTPS'])) {
+        $header = $_SERVER['X_PAPAYA_HTTPS'];
+      } elseif (isset($_SERVER['HTTP_X_PAPAYA_HTTPS'])) {
+        $header = $_SERVER['HTTP_X_PAPAYA_HTTPS'];
+      } else {
+        $header = NULL;
+      }
+      if (
+        NULL !== $header &&
+        \defined('PAPAYA_HEADER_HTTPS_TOKEN') &&
+        PAPAYA_HEADER_HTTPS_TOKEN === $header &&
+        32 === \strlen(PAPAYA_HEADER_HTTPS_TOKEN)
+      ) {
+        return TRUE;
+      }
+      return (isset($_SERVER['HTTPS']) && 'on' === \strtolower($_SERVER['HTTPS']));
+    }
+
+    /**
+     * Return the currently used protocol. "http" or "https".
+     *
+     * @param int $mode
+     *
+     * @return string
+     */
+    public static function get($mode = self::BOTH) {
+      if (self::BOTH !== $mode) {
+        return self::HTTPS === $mode ? 'https' : 'http';
+      }
+      return self::isSecure() ? 'https' : 'http';
+    }
+
+    /**
+     * Return the default port
+     *
+     * @return string
+     */
+    public static function getDefaultPort() {
+      return (isset($_SERVER['HTTPS']) && 'on' === \strtolower($_SERVER['HTTPS']))
+        ? 443 : 80;
+    }
   }
 }
