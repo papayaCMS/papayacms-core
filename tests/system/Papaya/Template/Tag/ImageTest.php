842ecd80c59e49f9a6a473f2d886bde93cb13c27
Thomas Weinert <thomas@weinert.info>
2019-10-30 20:56:12 +0100

    [TEST] Add Tests For Template\Tag\Image

--- tests/system/Papaya/Template/Tag/ImageTest.php
+++ tests/system/Papaya/Template/Tag/ImageTest.php
@@ -13,47 +13,109 @@
  *  FOR A PARTICULAR PURPOSE.
  */
 
-namespace Papaya\Template\Tag;
-require_once __DIR__.'/../../../../bootstrap.php';
+namespace Papaya\Template\Tag {
 
-class ImageTest extends \Papaya\TestCase {
+  use Papaya\TestCase;
+  use Papaya\XML\Document as XMLDocument;
+
+  require_once __DIR__.'/../../../../bootstrap.php';
+
   /**
-   * @covers \Papaya\Template\Tag\Image::appendTo
+   * @covers \Papaya\Template\Tag\Image
    */
-  public function testAppendTo() {
-    $image = new Image('d74f6d0324f5d90b23bb3771200ddf7d,60,96,max');
-    $document = new \Papaya\XML\Document();
-    $container = $document->appendElement('container');
-    $image->appendTo($container);
-    $this->assertXmlStringEqualsXmlString(
-    /** @lang XML */
-      '<container>
+  class ImageTest extends TestCase {
+
+    public function testAppendTo() {
+      $image = new Image('d74f6d0324f5d90b23bb3771200ddf7d,60,96,max');
+      $document = new XMLDocument();
+      $container = $document->appendElement('container');
+      $image->appendTo($container);
+      $this->assertXmlStringEqualsXmlString(
+      /** @lang XML */
+        '<container>
          <papaya:media 
            xmlns:papaya="http://www.papaya-cms.com/ns/papayacms" 
            src="d74f6d0324f5d90b23bb3771200ddf7d" width="60" height="96" resize="max"/>
        </container>',
-      $document->saveHTML()
-    );
-  }
+        $document->saveHTML()
+      );
+    }
 
-  /**
-   * @covers \Papaya\Template\Tag\Image::appendTo
-   */
-  public function testAppendToWithExistingTag() {
-    $image = new Image(
-      '<papaya:media src="d74f6d0324f5d90b23bb3771200ddf7d" width="60" height="96" resize="max"/>'
-    );
-    $document = new \Papaya\XML\Document();
-    $container = $document->appendElement('container');
-    $image->appendTo($container);
-    $this->assertXmlStringEqualsXmlString(
-    /** @lang XML */
-      '<container>
+    public function testAppendToWithExistingTag() {
+      $image = new Image(
+        '<papaya:media src="d74f6d0324f5d90b23bb3771200ddf7d" width="60" height="96" resize="max"/>'
+      );
+      $document = new XMLDocument();
+      $container = $document->appendElement('container');
+      $image->appendTo($container);
+      $this->assertXmlStringEqualsXmlString(
+      /** @lang XML */
+        '<container>
          <papaya:media 
            xmlns:papaya="http://www.papaya-cms.com/ns/papayacms" 
            src="d74f6d0324f5d90b23bb3771200ddf7d" width="60" height="96" resize="max"/>
        </container>',
-      $document->saveHTML()
-    );
+        $document->saveHTML()
+      );
+    }
+
+
+    public function testAppendToWithConstructorArguments() {
+      $image = new Image(
+        'd74f6d0324f5d90b23bb3771200ddf7d',
+        160,
+        196,
+        'alternative',
+        'min',
+        'a subtitle'
+      );
+      $document = new XMLDocument();
+      $container = $document->appendElement('container');
+      $image->appendTo($container);
+      $this->assertXmlStringEqualsXmlString(
+      /** @lang XML */
+        '<container>
+         <papaya:media 
+           xmlns:papaya="http://www.papaya-cms.com/ns/papayacms" 
+           src="d74f6d0324f5d90b23bb3771200ddf7d" 
+           width="160" 
+           height="196" 
+           resize="min" 
+           alt="alternative" 
+           subtitle="a subtitle"/>
+       </container>',
+        $document->saveHTML()
+      );
+    }
+
+    public function testAppendToWithEmptyMediaSource() {
+      $image = new Image('');
+      $document = new XMLDocument();
+      $container = $document->appendElement('container');
+      $image->appendTo($container);
+      $this->assertXmlStringEqualsXmlString(
+      /** @lang XML */
+        '<container/>',
+        $document->saveHTML()
+      );
+    }
+
+    public function testToString() {
+      $image = new Image('d74f6d0324f5d90b23bb3771200ddf7d,60,96,max');
+      $this->assertXmlStringEqualsXmlString(
+      /** @lang XML */
+        '<papaya:media 
+           xmlns:papaya="http://www.papaya-cms.com/ns/papayacms" 
+           src="d74f6d0324f5d90b23bb3771200ddf7d" width="60" height="96" resize="max"/>',
+        (string)$image
+      );
+    }
+
+    public function testToStringWithEmptyMediaSource() {
+      $image = new Image('');
+      $this->assertEmpty(
+        (string)$image
+      );
+    }
   }
 }
