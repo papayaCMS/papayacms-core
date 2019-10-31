de2e32d65e5d15a055b89728a196b6f31ba75081
Thomas Weinert <thomas@weinert.info>
2019-10-31 16:29:07 +0100

    [TEST] Add Tests For Filter\AssociativeArray

--- tests/system/Papaya/Filter/AssociativeArrayTest.php
+++ tests/system/Papaya/Filter/AssociativeArrayTest.php
@@ -13,132 +13,153 @@
  *  FOR A PARTICULAR PURPOSE.
  */
 
-namespace Papaya\Filter;
+namespace Papaya\Filter {
 
-require_once __DIR__.'/../../../bootstrap.php';
+  use InvalidArgumentException;
+  use Papaya\Filter;
+  use Papaya\TestCase;
 
-class AssociativeArrayTest extends \Papaya\TestCase {
-
-  /**
-   * @covers \Papaya\Filter\AssociativeArray
-   * @throws Exception
-   */
-  public function testValidateExpectingTrue() {
-    $subFilter = $this->getMockBuilder(\Papaya\Filter::class)->getMock();
-    $subFilter
-      ->expects($this->any())
-      ->method('validate')
-      ->willReturn(TRUE);
-    $filter = new AssociativeArray(
-      [
-        'foo' => $subFilter,
-        'bar' => $subFilter
-      ]
-    );
-    $this->assertTrue($filter->validate(['foo' => 21, 'bar' => 42]));
-  }
-
-  /**
-   * @covers \Papaya\Filter\AssociativeArray
-   * @throws Exception
-   */
-  public function testValidateInvalidElementValueExpectingException() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Exception $e */
-    $e = $this->createMock(Exception::class);
-    $subFilter = $this->getMockBuilder(\Papaya\Filter::class)->getMock();
-    $subFilter
-      ->expects($this->any())
-      ->method('validate')
-      ->willThrowException($e);
-    $filter = new AssociativeArray(
-      [
-        'foo' => $subFilter,
-        'bar' => $subFilter
-      ]
-    );
-    $this->expectException(Exception::class);
-    $this->assertTrue($filter->validate(['foo' => 21, 'bar' => 42]));
-  }
-
-  /**
-   * @covers \Papaya\Filter\AssociativeArray
-   * @throws Exception
-   */
-  public function testValidateInvalidKeyExpectingException() {
-    $subFilter = $this->getMockBuilder(\Papaya\Filter::class)->getMock();
-    $subFilter
-      ->expects($this->any())
-      ->method('validate')
-      ->willReturn(TRUE);
-    $filter = new AssociativeArray(
-      [
-        'foo' => $subFilter
-      ]
-    );
-    $this->expectException(Exception\InvalidKey::class);
-    $this->assertTrue($filter->validate(['foo' => 21, 'bar' => 42]));
-  }
+  require_once __DIR__.'/../../../bootstrap.php';
 
   /**
    * @covers \Papaya\Filter\AssociativeArray
    */
-  public function testFilterExpectingValue() {
-    $subFilter = $this->getMockBuilder(\Papaya\Filter::class)->getMock();
-    $subFilter
-      ->expects($this->any())
-      ->method('filter')
-      ->willReturnArgument(0);
-    $filter = new AssociativeArray(
-      [
-        'foo' => $subFilter,
-        'bar' => $subFilter
-      ]
-    );
-    $this->assertEquals(
-      ['foo' => 21, 'bar' => 42],
-      $filter->filter(['foo' => 21, 'bar' => 42])
-    );
-  }
+  class AssociativeArrayTest extends TestCase {
 
-  /**
-   * @covers \Papaya\Filter\AssociativeArray
-   */
-  public function testFilterExpectingNull() {
-    $subFilter = $this->getMockBuilder(\Papaya\Filter::class)->getMock();
-    $subFilter
-      ->expects($this->any())
-      ->method('filter')
-      ->willReturn(NULL);
-    $filter = new AssociativeArray(
-      [
-        'foo' => $subFilter,
-        'bar' => $subFilter
-      ]
-    );
-    $this->assertEquals(
-      [],
-      $filter->filter(['foo' => 21, 'bar' => 42])
-    );
-  }
+    public function testConstructorWithEmptyDefinitionArrayExceptionException() {
+      $this->expectException(InvalidArgumentException::class);
+      new AssociativeArray([]);
+    }
 
-  /**
-   * @covers \Papaya\Filter\AssociativeArray
-   */
-  public function testFilterWithoutArrayExpectingNull() {
-    $subFilter = $this->getMockBuilder(\Papaya\Filter::class)->getMock();
-    $subFilter
-      ->expects($this->any())
-      ->method('filter')
-      ->willReturn(TRUE);
-    $filter = new AssociativeArray(
-      [
-        'foo' => $subFilter,
-        'bar' => $subFilter
-      ]
-    );
-    $this->assertNull(
-      $filter->filter(42)
-    );
-  }
+    public function testConstructorWithInvalidDefinitionArrayExceptionException() {
+      $this->expectException(InvalidArgumentException::class);
+      new AssociativeArray(['foo' => 'not a filter']);
+    }
 
+    /**
+     * @throws Exception
+     */
+    public function testValidateExpectingTrue() {
+      $subFilter = $this->getMockBuilder(Filter::class)->getMock();
+      $subFilter
+        ->method('validate')
+        ->willReturn(TRUE);
+      $filter = new AssociativeArray(
+        [
+          'foo' => $subFilter,
+          'bar' => $subFilter
+        ]
+      );
+      $this->assertTrue($filter->validate(['foo' => 21, 'bar' => 42]));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testValidateInvalidElementValueExpectingException() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Exception $e */
+      $e = $this->createMock(Exception::class);
+      $subFilter = $this->getMockBuilder(Filter::class)->getMock();
+      $subFilter
+        ->method('validate')
+        ->willThrowException($e);
+      $filter = new AssociativeArray(
+        [
+          'foo' => $subFilter,
+          'bar' => $subFilter
+        ]
+      );
+      $this->expectException(Exception::class);
+      $this->assertTrue($filter->validate(['foo' => 21, 'bar' => 42]));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testValidateInvalidValueTypeExpectingException() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Exception $e */
+      $e = $this->createMock(Exception::class);
+      $subFilter = $this->getMockBuilder(Filter::class)->getMock();
+      $subFilter
+        ->expects($this->never())
+        ->method('validate');
+      $filter = new AssociativeArray(
+        [
+          'foo' => $subFilter,
+          'bar' => $subFilter
+        ]
+      );
+      $this->expectException(Exception::class);
+      $this->assertTrue($filter->validate('invalid-type'));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testValidateInvalidKeyExpectingException() {
+      $subFilter = $this->getMockBuilder(Filter::class)->getMock();
+      $subFilter
+        ->method('validate')
+        ->willReturn(TRUE);
+      $filter = new AssociativeArray(
+        [
+          'foo' => $subFilter
+        ]
+      );
+      $this->expectException(Exception\InvalidKey::class);
+      $this->assertTrue($filter->validate(['foo' => 21, 'bar' => 42]));
+    }
+
+    public function testFilterExpectingValue() {
+      $subFilter = $this->getMockBuilder(Filter::class)->getMock();
+      $subFilter
+        ->method('filter')
+        ->willReturnArgument(0);
+      $filter = new AssociativeArray(
+        [
+          'foo' => $subFilter,
+          'bar' => $subFilter
+        ]
+      );
+      $this->assertEquals(
+        ['foo' => 21, 'bar' => 42],
+        $filter->filter(['foo' => 21, 'bar' => 42])
+      );
+    }
+
+    public function testFilterExpectingNull() {
+      $subFilter = $this->getMockBuilder(Filter::class)->getMock();
+      $subFilter
+        ->expects($this->any())
+        ->method('filter')
+        ->willReturn(NULL);
+      $filter = new AssociativeArray(
+        [
+          'foo' => $subFilter,
+          'bar' => $subFilter
+        ]
+      );
+      $this->assertEquals(
+        [],
+        $filter->filter(['foo' => 21, 'bar' => 42])
+      );
+    }
+
+    public function testFilterWithoutArrayExpectingNull() {
+      $subFilter = $this->getMockBuilder(Filter::class)->getMock();
+      $subFilter
+        ->expects($this->any())
+        ->method('filter')
+        ->willReturn(TRUE);
+      $filter = new AssociativeArray(
+        [
+          'foo' => $subFilter,
+          'bar' => $subFilter
+        ]
+      );
+      $this->assertNull(
+        $filter->filter(42)
+      );
+    }
+  }
 }
