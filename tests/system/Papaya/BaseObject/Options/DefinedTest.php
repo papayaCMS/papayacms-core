9b2ace9f3840131e9687069c23b3b96b4815421e
Thomas Weinert <thomas@weinert.info>
2019-10-31 17:38:36 +0100

    [CLEANUP] Cleanup BaseObject\Options\DefinedTest

--- tests/system/Papaya/BaseObject/Options/DefinedTest.php
+++ tests/system/Papaya/BaseObject/Options/DefinedTest.php
@@ -15,49 +15,42 @@
 
 namespace Papaya\BaseObject\Options {
 
+  use Papaya\TestCase;
+
   require_once __DIR__.'/../../../../bootstrap.php';
 
-  class DefinedTest extends \Papaya\TestCase {
+  /**
+   * @covers \Papaya\BaseObject\Options\Defined
+   */
+  class DefinedTest extends TestCase {
 
-    /**
-     * @covers \Papaya\BaseObject\Options\Defined::toArray
-     */
     public function testToArray() {
       $options = new Defined_TestProxy();
       $this->assertEquals(
-        array(
+        [
           'VALID_OPTION' => TRUE
-        ),
+        ],
         $options->toArray()
       );
     }
 
-    /**
-     * @covers \Papaya\BaseObject\Options\Defined::count
-     */
     public function testCount() {
       $options = new Defined_TestProxy();
       $this->assertCount(1, $options);
     }
 
-    /**
-     * @covers \Papaya\BaseObject\Options\Defined::_write
-     */
     public function testSetOption() {
       $options = new Defined_TestProxy();
       $options->validOption = FALSE;
       $this->assertAttributeEquals(
-        array(
+        [
           'VALID_OPTION' => FALSE
-        ),
+        ],
         '_options',
         $options
       );
     }
 
-    /**
-     * @covers \Papaya\BaseObject\Options\Defined::_write
-     */
     public function testSetOptionExpectingException() {
       $options = new Defined_TestProxy();
       $this->expectException(\InvalidArgumentException::class);
@@ -65,26 +58,17 @@ namespace Papaya\BaseObject\Options {
       $options->invalidOption = FALSE;
     }
 
-    /**
-     * @covers \Papaya\BaseObject\Options\Defined::_read
-     */
     public function testGetOptionAfterSet() {
       $options = new Defined_TestProxy();
       $options->validOption = FALSE;
       $this->assertFalse($options->validOption);
     }
 
-    /**
-     * @covers \Papaya\BaseObject\Options\Defined::_read
-     */
     public function testGetOptionReadingDefault() {
       $options = new Defined_TestProxy();
       $this->assertTrue($options->validOption);
     }
 
-    /**
-     * @covers \Papaya\BaseObject\Options\Defined::_read
-     */
     public function testGetOptionExpectingException() {
       $options = new Defined_TestProxy();
       $this->expectException(\InvalidArgumentException::class);
@@ -92,17 +76,11 @@ namespace Papaya\BaseObject\Options {
       $options->invalidOption;
     }
 
-    /**
-     * @covers \Papaya\BaseObject\Options\Defined::_exists
-     */
     public function testIssetOptionExpectingTrue() {
       $options = new Defined_TestProxy();
       $this->assertTrue(isset($options->validOption));
     }
 
-    /**
-     * @covers \Papaya\BaseObject\Options\Defined::_exists
-     */
     public function testIssetOptionExpectingFalse() {
       $options = new Defined_TestProxy();
       $this->assertFalse(isset($options->invalidOption));
@@ -117,9 +95,9 @@ namespace Papaya\BaseObject\Options {
 
     public function __construct(array $options = NULL) {
       parent::__construct(
-        array(
-          'VALID_OPTION' => array(TRUE, FALSE)
-        ),
+        [
+          'VALID_OPTION' => [TRUE, FALSE]
+        ],
         $options
       );
     }
