96ededa616b3bbab8f03bf395f7be0db6a346ac4
Thomas Weinert <thomas@weinert.info>
2019-10-31 16:40:02 +0100

    [TEST] Add Tests For Filter\Identifier

--- tests/system/Papaya/Filter/IdentifierTest.php
+++ tests/system/Papaya/Filter/IdentifierTest.php
@@ -13,84 +13,117 @@
  *  FOR A PARTICULAR PURPOSE.
  */
 
-namespace Papaya\Filter;
+namespace Papaya\Filter {
 
-require_once __DIR__.'/../../../bootstrap.php';
+  use InvalidArgumentException;
+  use Papaya\TestCase;
 
-class IdentifierTest extends \Papaya\TestCase {
-
-  /**
-   * @covers \Papaya\Filter\Length::__construct
-   * @dataProvider provideValuesForFilter
-   * @param string|NULL $expected
-   * @param mixed $input
-   * @param int $minimum
-   * @param int $maximum
-   * @param int $mode
-   */
-  public function testFilter($expected, $input, $minimum = 0, $maximum = 0, $mode = Identifier::CASE_INSENSITIVE) {
-    $filter = new Identifier($minimum, $maximum, $mode);
-    $this->assertEquals($expected, $filter->filter($input));
-  }
+  require_once __DIR__.'/../../../bootstrap.php';
 
   /**
-   * @covers \Papaya\Filter\Length::__construct
-   * @dataProvider provideValuesForValidateExpectingTrue
-   * @param mixed $input
-   * @param int $minimum
-   * @param int $maximum
-   * @param int $mode
-   * @throws Exception
+   * @covers \Papaya\Filter\Identifier
    */
-  public function testValidateExpectingTrue($input, $minimum = 0, $maximum = 0, $mode = Identifier::CASE_INSENSITIVE) {
-    $filter = new Identifier($minimum, $maximum, $mode);
-    $this->assertTrue($filter->validate($input));
-  }
+  class IdentifierTest extends TestCase {
 
-  /**
-   * @covers \Papaya\Filter\Length::__construct
-   * @dataProvider provideValuesForValidateExpectingException
-   * @param mixed $input
-   * @param int $minimum
-   * @param int $maximum
-   * @param int $mode
-   * @throws Exception
-   */
-  public function testValidateExpectingException($input, $minimum = 0, $maximum = 0, $mode = Identifier::CASE_INSENSITIVE) {
-    $filter = new Identifier($minimum, $maximum, $mode);
-    $this->expectException(Exception\InvalidValue::class);
-    $this->assertTrue($filter->validate($input));
-  }
+    public function testConstructorWithInvalidMinimumExpectingException() {
+      $this->expectException(InvalidArgumentException::class);
+      new Identifier(-1);
+    }
 
-  public function provideValuesForFilter() {
-    return [
-      ['foo', 'foo'],
-      [NULL, '$$$'],
-      ['foo', 'foo', 1, 3],
-      ['foo', 'foobar', 1, 3],
-      [NULL, 'foo', 4, 4],
-      ['FOO', 'foo', 1, 3, Identifier::UPPERCASE],
-      ['foo', 'FOO', 1, 3, Identifier::LOWERCASE],
-      ['foo', 'f$o$o$']
-    ];
-  }
+    public function testConstructorWithInvalidMaximumExpectingException() {
+      $this->expectException(InvalidArgumentException::class);
+      new Identifier(1, -1);
+    }
 
-  public function provideValuesForValidateExpectingTrue() {
-    return [
-      ['foo'],
-      ['foo', 1, 3],
-      ['FOO', 1, 3, Identifier::UPPERCASE],
-      ['foo', 1, 3, Identifier::LOWERCASE]
-    ];
-  }
+    public function testConstructorWithMinimumLargerMaximumExpectingException() {
+      $this->expectException(InvalidArgumentException::class);
+      new Identifier(10, 5);
+    }
 
-  public function provideValuesForValidateExpectingException() {
-    return [
-      ['$$$'],
-      ['foo', 4, 4],
-      ['foobar', 1, 3],
-      ['foo', 1, 3, Identifier::UPPERCASE],
-      ['FOO', 1, 3, Identifier::LOWERCASE]
-    ];
+    /**
+     * @dataProvider provideValuesForFilter
+     * @param string|NULL $expected
+     * @param mixed $input
+     * @param int $minimum
+     * @param int $maximum
+     * @param int $mode
+     */
+    public function testFilter($expected, $input, $minimum = 0, $maximum = 0, $mode = Identifier::CASE_INSENSITIVE) {
+      $filter = new Identifier($minimum, $maximum, $mode);
+      $this->assertEquals($expected, $filter->filter($input));
+    }
+
+    /**
+     * @dataProvider provideValuesForValidateExpectingTrue
+     * @param mixed $input
+     * @param int $minimum
+     * @param int $maximum
+     * @param int $mode
+     * @throws Exception
+     */
+    public function testValidateExpectingTrue($input, $minimum = 0, $maximum = 0, $mode = Identifier::CASE_INSENSITIVE
+    ) {
+      $filter = new Identifier($minimum, $maximum, $mode);
+      $this->assertTrue($filter->validate($input));
+    }
+
+    /**
+     * @dataProvider provideValuesForValidateExpectingException
+     * @param mixed $input
+     * @param int $minimum
+     * @param int $maximum
+     * @param int $mode
+     * @throws Exception
+     */
+    public function testValidateExpectingException(
+      $input, $minimum = 0, $maximum = 0, $mode = Identifier::CASE_INSENSITIVE
+    ) {
+      $filter = new Identifier($minimum, $maximum, $mode);
+      $this->expectException(Exception\InvalidValue::class);
+      $this->assertTrue($filter->validate($input));
+    }
+
+    /**
+     * @throws Exception\InvalidValue
+     * @throws Exception\UnexpectedType
+     */
+    public function testValidateWithInvalidValueTypeExpectingException(
+    ) {
+      $filter = new Identifier();
+      $this->expectException(Exception\UnexpectedType::class);
+      $this->assertTrue($filter->validate([]));
+    }
+
+    public function provideValuesForFilter() {
+      return [
+        ['foo', 'foo'],
+        [NULL, '$$$'],
+        ['foo', 'foo', 1, 3],
+        ['foo', 'foobar', 1, 3],
+        [NULL, 'foo', 4, 4],
+        ['FOO', 'foo', 1, 3, Identifier::UPPERCASE],
+        ['foo', 'FOO', 1, 3, Identifier::LOWERCASE],
+        ['foo', 'f$o$o$']
+      ];
+    }
+
+    public function provideValuesForValidateExpectingTrue() {
+      return [
+        ['foo'],
+        ['foo', 1, 3],
+        ['FOO', 1, 3, Identifier::UPPERCASE],
+        ['foo', 1, 3, Identifier::LOWERCASE]
+      ];
+    }
+
+    public function provideValuesForValidateExpectingException() {
+      return [
+        ['$$$'],
+        ['foo', 4, 4],
+        ['foobar', 1, 3],
+        ['foo', 1, 3, Identifier::UPPERCASE],
+        ['FOO', 1, 3, Identifier::LOWERCASE]
+      ];
+    }
   }
 }
