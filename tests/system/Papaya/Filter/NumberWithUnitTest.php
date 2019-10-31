07b0c9aa4ea993c884d713c7e3f5a8c5ce6c35de
Thomas Weinert <thomas@weinert.info>
2019-10-31 17:28:58 +0100

    [CLEANUP] Cleanup Filter\NumberWithUnitTest

--- tests/system/Papaya/Filter/NumberWithUnitTest.php
+++ tests/system/Papaya/Filter/NumberWithUnitTest.php
@@ -13,182 +13,141 @@
  *  FOR A PARTICULAR PURPOSE.
  */
 
-namespace Papaya\Filter;
-require_once __DIR__.'/../../../bootstrap.php';
-
-class NumberWithUnitTest extends \Papaya\TestCase {
+namespace Papaya\Filter {
+  require_once __DIR__.'/../../../bootstrap.php';
 
   /**
-   * @covers \Papaya\Filter\NumberWithUnit::__construct
-   * @dataProvider providerConstructArguments
-   * @param string|array $units
-   * @param array $expectedUnits
-   * @param float $minimum
-   * @param float $maximum
-   * @param string $algebraicSign
+   * @covers \Papaya\Filter\NumberWithUnit
    */
-  public function testConstruct($units, $expectedUnits, $minimum, $maximum, $algebraicSign) {
-    $filter = new NumberWithUnit($units, $minimum, $maximum, $algebraicSign);
-    $this->assertAttributeEquals($expectedUnits, '_units', $filter);
-    $this->assertAttributeEquals($minimum, '_minimum', $filter);
-    $this->assertAttributeEquals($maximum, '_maximum', $filter);
-    $this->assertAttributeEquals($algebraicSign, '_algebraicSign', $filter);
+  class NumberWithUnitTest extends \Papaya\TestCase {
+
+    /**
+     * @dataProvider providerConstructArguments
+     * @param string|array $units
+     * @param array $expectedUnits
+     * @param float $minimum
+     * @param float $maximum
+     * @param string $algebraicSign
+     */
+    public function testConstruct($units, $expectedUnits, $minimum, $maximum, $algebraicSign) {
+      $filter = new NumberWithUnit($units, $minimum, $maximum, $algebraicSign);
+      $this->assertAttributeEquals($expectedUnits, '_units', $filter);
+      $this->assertAttributeEquals($minimum, '_minimum', $filter);
+      $this->assertAttributeEquals($maximum, '_maximum', $filter);
+      $this->assertAttributeEquals($algebraicSign, '_algebraicSign', $filter);
+    }
+
+    public function testConstructOnlyUnit() {
+      $filter = new NumberWithUnit('em');
+      $this->assertAttributeEquals(['em'], '_units', $filter);
+    }
+
+    public function testConstructOnlyUnitAndMin() {
+      $filter = new NumberWithUnit('px', 5);
+      $this->assertAttributeEquals(['px'], '_units', $filter);
+      $this->assertAttributeEquals(5, '_minimum', $filter);
+    }
+
+    public function testConstructOnlyUnitAndMinAndMax() {
+      $filter = new NumberWithUnit(['em', 'pt'], 81, 999, '-');
+      $this->assertAttributeEquals(['em', 'pt'], '_units', $filter);
+      $this->assertAttributeEquals(81, '_minimum', $filter);
+      $this->assertAttributeEquals(999, '_maximum', $filter);
+      $this->assertAttributeEquals('-', '_algebraicSign', $filter);
+    }
+
+    public function testConstructOnlyUnitAndMinAndMaxAndAlgebraicSign() {
+      $filter = new NumberWithUnit(['%', 'pt'], -34, 91);
+      $this->assertAttributeEquals(['%', 'pt'], '_units', $filter);
+      $this->assertAttributeEquals(-34, '_minimum', $filter);
+      $this->assertAttributeEquals(91, '_maximum', $filter);
+    }
+
+    public function testConstructExceptionMissingUnit() {
+      $this->expectException(\UnexpectedValueException::class);
+      new NumberWithUnit('');
+    }
+
+    public function testValidate() {
+      $filter = new NumberWithUnit('px', 0, 100);
+      $this->assertTrue($filter->validate('10px'));
+    }
+
+    public function testValidateWithZero() {
+      $filter = new NumberWithUnit('px', 0, 100);
+      $this->assertTrue($filter->validate('0'));
+    }
+
+    public function testValidateExpectedFilterNotEnclosedException() {
+      $filter = new NumberWithUnit('px');
+      $this->expectException(Exception\NotIncluded::class);
+      $filter->validate('99abc');
+    }
+
+    public function testValidateExpectedCharacterInvalidExceptionNegativeValue() {
+      $filter = new NumberWithUnit('em', 1, 1000, '-');
+      $this->expectException(Exception\InvalidCharacter::class);
+      $filter->validate('999em');
+    }
+
+    public function testValidateExpectedCharacterInvalidExceptionPositiveValue() {
+      $filter = new NumberWithUnit('em', 1, 1000, '+');
+      $this->expectException(Exception\InvalidCharacter::class);
+      $filter->validate('-999em');
+    }
+
+    public function testValidateExpectedFilterRangeMinimumException() {
+      $filter = new NumberWithUnit('px', -10);
+      $this->expectException(Exception\OutOfRange\ToSmall::class);
+      $filter->validate('-999px');
+    }
+
+    public function testValidateExpectedFilterRangeMaximumException() {
+      $filter = new NumberWithUnit('px', -10, 999);
+      $this->expectException(Exception\OutOfRange\ToLarge::class);
+      $filter->validate('1000px');
+    }
+
+    /**
+     * @dataProvider providerFilter
+     * @param string|NULL $expects
+     * @param mixed $value
+     */
+    public function testFilter($expects, $value) {
+      $filter = new NumberWithUnit('px');
+      $this->assertEquals($expects, $filter->filter($value));
+    }
+
+    public function testFilterExpectsFilteredResult() {
+      $filter = new NumberWithUnit('px');
+      $this->assertEquals('102,324234.23px', $filter->filter(' 102,324234.23 px '));
+    }
+
+    public function testFilterExpectsNull() {
+      $filter = new NumberWithUnit('px');
+      $this->assertNull($filter->filter(' 102,324234dasda23px '));
+    }
+
+    /************************
+     * Data Provider
+     ************************/
+
+    public static function providerConstructArguments() {
+      return [
+        ['em', ['em'], -5, 82, NULL],
+        [['pt', 'px'], ['pt', 'px'], 11, 965, '-'],
+      ];
+    }
+
+    public static function providerFilter() {
+      return [
+        ['10px', 'ooijoijdiooi jgroj10iubweuifbiubuiwb fwiupxjnqjknjqwndjn   '],
+        ['10,123.91px', 'ooijoijdiooi jgroj10,123.91iubweuifbiubuiwb fwiupxjnqjknjqwndjn   '],
+        ['-82,992.93px', '    <b>-82,992.93px</b>'],
+        ['992.93px', '    <b>992.93px</b>'],
+        ['0', '0'],
+      ];
+    }
+
   }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::__construct
-   */
-  public function testConstructOnlyUnit() {
-    $filter = new NumberWithUnit('em');
-    $this->assertAttributeEquals(array('em'), '_units', $filter);
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::__construct
-   */
-  public function testConstructOnlyUnitAndMin() {
-    $filter = new NumberWithUnit('px', 5);
-    $this->assertAttributeEquals(array('px'), '_units', $filter);
-    $this->assertAttributeEquals(5, '_minimum', $filter);
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::__construct
-   */
-  public function testConstructOnlyUnitAndMinAndMax() {
-    $filter = new NumberWithUnit(array('em', 'pt'), 81, 999, '-');
-    $this->assertAttributeEquals(array('em', 'pt'), '_units', $filter);
-    $this->assertAttributeEquals(81, '_minimum', $filter);
-    $this->assertAttributeEquals(999, '_maximum', $filter);
-    $this->assertAttributeEquals('-', '_algebraicSign', $filter);
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::__construct
-   */
-  public function testConstructOnlyUnitAndMinAndMaxAndAlgebraicSign() {
-    $filter = new NumberWithUnit(array('%', 'pt'), -34, 91);
-    $this->assertAttributeEquals(array('%', 'pt'), '_units', $filter);
-    $this->assertAttributeEquals(-34, '_minimum', $filter);
-    $this->assertAttributeEquals(91, '_maximum', $filter);
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::__construct
-   */
-  public function testConstructExceptionMissingUnit() {
-    $this->expectException(\UnexpectedValueException::class);
-    new NumberWithUnit('');
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::validate
-   */
-  public function testValidate() {
-    $filter = new NumberWithUnit('px', 0, 100);
-    $this->assertTrue($filter->validate('10px'));
-  }
-
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::validate
-   */
-  public function testValidateWithZero() {
-    $filter = new NumberWithUnit('px', 0, 100);
-    $this->assertTrue($filter->validate('0'));
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::validate
-   */
-  public function testValidateExpectedFilterNotEnclosedException() {
-    $filter = new NumberWithUnit('px');
-    $this->expectException(Exception\NotIncluded::class);
-    $filter->validate('99abc');
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::validate
-   */
-  public function testValidateExpectedCharacterInvalidExceptionNegativeValue() {
-    $filter = new NumberWithUnit('em', 1, 1000, '-');
-    $this->expectException(Exception\InvalidCharacter::class);
-    $filter->validate('999em');
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::validate
-   */
-  public function testValidateExpectedCharacterInvalidExceptionPositveValue() {
-    $filter = new NumberWithUnit('em', 1, 1000, '+');
-    $this->expectException(Exception\InvalidCharacter::class);
-    $filter->validate('-999em');
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::validate
-   */
-  public function testValidateExpectedFilterRangeMinimumException() {
-    $filter = new NumberWithUnit('px', -10);
-    $this->expectException(Exception\OutOfRange\ToSmall::class);
-    $filter->validate('-999px');
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::validate
-   */
-  public function testValidateExpectedFilterRangeMaximumException() {
-    $filter = new NumberWithUnit('px', -10, 999);
-    $this->expectException(Exception\OutOfRange\ToLarge::class);
-    $filter->validate('1000px');
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::filter
-   * @dataProvider providerFilter
-   * @param string|NULL $expects
-   * @param mixed $value
-   */
-  public function testFilter($expects, $value) {
-    $filter = new NumberWithUnit('px');
-    $this->assertEquals($expects, $filter->filter($value));
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::filter
-   */
-  public function testFilterExpectsFilteredResult() {
-    $filter = new NumberWithUnit('px');
-    $this->assertEquals('102,324234.23px', $filter->filter(' 102,324234.23 px '));
-  }
-
-  /**
-   * @covers \Papaya\Filter\NumberWithUnit::filter
-   */
-  public function testFilterExpectsNull() {
-    $filter = new NumberWithUnit('px');
-    $this->assertNull($filter->filter(' 102,324234dasda23px '));
-  }
-
-  /************************
-   * Data Provider
-   ************************/
-
-  public static function providerConstructArguments() {
-    return array(
-      array('em', array('em'), -5, 82, NULL),
-      array(array('pt', 'px'), array('pt', 'px'), 11, 965, '-'),
-    );
-  }
-
-  public static function providerFilter() {
-    return array(
-      array('10px', 'ooijoijdiooi jgroj10iubweuifbiubuiwb fwiupxjnqjknjqwndjn   '),
-      array('10,123.91px', 'ooijoijdiooi jgroj10,123.91iubweuifbiubuiwb fwiupxjnqjknjqwndjn   '),
-      array('-82,992.93px', '    <b>-82,992.93px</b>'),
-      array('992.93px', '    <b>992.93px</b>'),
-      array('0', '0'),
-    );
-  }
-
 }
