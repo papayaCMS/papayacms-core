2b8747a88e740b7c355a937769323b8b2ef801dd
Thomas Weinert <thomas@weinert.info>
2019-11-04 15:12:47 +0100

    [TEST] Add Tests For Message\Manager

--- tests/system/Papaya/Message/ManagerTest.php
+++ tests/system/Papaya/Message/ManagerTest.php
@@ -13,314 +13,286 @@
  *  FOR A PARTICULAR PURPOSE.
  */
 
-namespace Papaya\Message;
-require_once __DIR__.'/../../../bootstrap.php';
+namespace Papaya\Message {
 
-class ManagerTest extends \Papaya\TestCase {
+  use Papaya\Template;
+  use Papaya\TestCase;
 
-  /**
-   * @covers \Papaya\Message\Manager::addDispatcher
-   */
-  public function testAddDispatcher() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
-    $dispatcher = $this->createMock(Dispatcher::class);
-    $manager = new Manager();
-    $manager->addDispatcher($dispatcher);
-    $this->assertAttributeEquals(
-      array($dispatcher),
-      '_dispatchers',
-      $manager
-    );
-  }
+  require_once __DIR__.'/../../../bootstrap.php';
 
   /**
-   * @covers \Papaya\Message\Manager::dispatch
+   * @covers \Papaya\Message\Manager
    */
-  public function testDispatch() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|\Papaya\Message $message */
-    $message = $this->createMock(\Papaya\Message::class);
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
-    $dispatcher = $this->createMock(Dispatcher::class);
-    $dispatcher
-      ->expects($this->once())
-      ->method('dispatch')
-      ->with($this->equalTo($message));
-    $manager = new Manager();
-    $manager->addDispatcher($dispatcher);
-    $manager->dispatch($message);
-  }
+  class ManagerTest extends TestCase {
 
-  /**
-   * @covers \Papaya\Message\Manager::display
-   */
-  public function testDisplay() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
-    $dispatcher = $this->createMock(Dispatcher::class);
-    $dispatcher
-      ->expects($this->once())
-      ->method('dispatch')
-      ->with($this->isInstanceOf(Display\Translated::class));
-    $manager = new Manager();
-    $manager->addDispatcher($dispatcher);
-    $manager->display(\Papaya\Message::SEVERITY_INFO, 'TEST');
-  }
-
-  /**
-   * @covers \Papaya\Message\Manager::displayInfo
-   */
-  public function testDisplayInfo() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
-    $dispatcher = $this->createMock(Dispatcher::class);
-    $dispatcher
-      ->expects($this->once())
-      ->method('dispatch')
-      ->with(
-        $this->callback(
-          function(Display\Translated $message) {
-            return
-              \Papaya\Message::SEVERITY_INFO === $message->getSeverity() &&
-              'TEST' === $message->getMessage();
-          }
-        )
+    public function testAddDispatcher() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
+      $dispatcher = $this->createMock(Dispatcher::class);
+      $manager = new Manager();
+      $manager->addDispatcher($dispatcher);
+      $this->assertAttributeEquals(
+        [$dispatcher],
+        '_dispatchers',
+        $manager
       );
-    $manager = new Manager();
-    $manager->addDispatcher($dispatcher);
-    $manager->displayInfo('TEST');
-  }
+    }
 
-  /**
-   * @covers \Papaya\Message\Manager::displayWarning
-   */
-  public function testDisplayWarning() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
-    $dispatcher = $this->createMock(Dispatcher::class);
-    $dispatcher
-      ->expects($this->once())
-      ->method('dispatch')
-      ->with(
-        $this->callback(
-          function(Display\Translated $message) {
-            return
-              \Papaya\Message::SEVERITY_WARNING === $message->getSeverity() &&
-              'TEST' === $message->getMessage();
-          }
-        )
+    public function testDispatch() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|\Papaya\Message $message */
+      $message = $this->createMock(\Papaya\Message::class);
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
+      $dispatcher = $this->createMock(Dispatcher::class);
+      $dispatcher
+        ->expects($this->once())
+        ->method('dispatch')
+        ->with($this->equalTo($message));
+      $manager = new Manager();
+      $manager->addDispatcher($dispatcher);
+      $manager->dispatch($message);
+    }
+
+    public function testDisplay() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
+      $dispatcher = $this->createMock(Dispatcher::class);
+      $dispatcher
+        ->expects($this->once())
+        ->method('dispatch')
+        ->with($this->isInstanceOf(Display\Translated::class));
+      $manager = new Manager();
+      $manager->addDispatcher($dispatcher);
+      $manager->display(\Papaya\Message::SEVERITY_INFO, 'TEST');
+    }
+
+    public function testDisplayInfo() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
+      $dispatcher = $this->createMock(Dispatcher::class);
+      $dispatcher
+        ->expects($this->once())
+        ->method('dispatch')
+        ->with(
+          $this->callback(
+            function (Display\Translated $message) {
+              return
+                \Papaya\Message::SEVERITY_INFO === $message->getSeverity() &&
+                'TEST' === $message->getMessage();
+            }
+          )
+        );
+      $manager = new Manager();
+      $manager->addDispatcher($dispatcher);
+      $manager->displayInfo('TEST');
+    }
+
+    public function testDisplayWarning() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
+      $dispatcher = $this->createMock(Dispatcher::class);
+      $dispatcher
+        ->expects($this->once())
+        ->method('dispatch')
+        ->with(
+          $this->callback(
+            function (Display\Translated $message) {
+              return
+                \Papaya\Message::SEVERITY_WARNING === $message->getSeverity() &&
+                'TEST' === $message->getMessage();
+            }
+          )
+        );
+      $manager = new Manager();
+      $manager->addDispatcher($dispatcher);
+      $manager->displayWarning('TEST');
+    }
+
+    public function testDisplayError() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
+      $dispatcher = $this->createMock(Dispatcher::class);
+      $dispatcher
+        ->expects($this->once())
+        ->method('dispatch')
+        ->with(
+          $this->callback(
+            function (Display\Translated $message) {
+              return
+                \Papaya\Message::SEVERITY_ERROR === $message->getSeverity() &&
+                'TEST' === $message->getMessage();
+            }
+          )
+        );
+      $manager = new Manager();
+      $manager->addDispatcher($dispatcher);
+      $manager->displayError('TEST');
+    }
+
+    public function testLog() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
+      $dispatcher = $this->createMock(Dispatcher::class);
+      $dispatcher
+        ->expects($this->once())
+        ->method('dispatch')
+        ->with(
+          new Log(
+            \Papaya\Message::SEVERITY_INFO,
+            Logable::GROUP_COMMUNITY,
+            'TEST'
+          )
+        );
+      $manager = new Manager();
+      $manager->addDispatcher($dispatcher);
+      $manager->log(\Papaya\Message::SEVERITY_INFO, Logable::GROUP_COMMUNITY, 'TEST');
+    }
+
+    public function testLogWithContext() {
+      $message = new Log(
+        \Papaya\Message::SEVERITY_INFO, Logable::GROUP_COMMUNITY, 'TEST'
       );
-    $manager = new Manager();
-    $manager->addDispatcher($dispatcher);
-    $manager->displayWarning('TEST');
-  }
-
-  /**
-   * @covers \Papaya\Message\Manager::displayError
-   */
-  public function testDisplayError() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
-    $dispatcher = $this->createMock(Dispatcher::class);
-    $dispatcher
-      ->expects($this->once())
-      ->method('dispatch')
-      ->with(
-        $this->callback(
-          function(Display\Translated $message) {
-            return
-              \Papaya\Message::SEVERITY_ERROR === $message->getSeverity() &&
-              'TEST' === $message->getMessage();
-          }
-        )
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Context\Data $context */
+      $context = $this->createMock(Context\Data::class);
+      $message->context()->append($context);
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
+      $dispatcher = $this->createMock(Dispatcher::class);
+      $dispatcher
+        ->expects($this->once())
+        ->method('dispatch')
+        ->with($message);
+      $manager = new Manager();
+      $manager->addDispatcher($dispatcher);
+      $manager->log(
+        \Papaya\Message::SEVERITY_INFO,
+        Logable::GROUP_COMMUNITY,
+        'TEST',
+        $context
       );
-    $manager = new Manager();
-    $manager->addDispatcher($dispatcher);
-    $manager->displayError('TEST');
-  }
+    }
 
-  /**
-   * @covers \Papaya\Message\Manager::log
-   */
-  public function testLog() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
-    $dispatcher = $this->createMock(Dispatcher::class);
-    $dispatcher
-      ->expects($this->once())
-      ->method('dispatch')
-      ->with(
-        new Log(
-          \Papaya\Message::SEVERITY_INFO,
-          Logable::GROUP_COMMUNITY,
-          'TEST'
-        )
+    public function testLogWithContextGroup() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Context\Group $context */
+      $context = $this->createMock(Context\Group::class);
+      $message = new Log(
+        \Papaya\Message::SEVERITY_INFO, Logable::GROUP_COMMUNITY, 'TEST'
       );
-    $manager = new Manager();
-    $manager->addDispatcher($dispatcher);
-    $manager->log(\Papaya\Message::SEVERITY_INFO, Logable::GROUP_COMMUNITY, 'TEST');
-  }
+      $message->setContext($context);
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
+      $dispatcher = $this->createMock(Dispatcher::class);
+      $dispatcher
+        ->expects($this->once())
+        ->method('dispatch')
+        ->with($message);
+      $manager = new Manager();
+      $manager->addDispatcher($dispatcher);
+      $manager->log(
+        \Papaya\Message::SEVERITY_INFO,
+        Logable::GROUP_COMMUNITY,
+        'TEST',
+        $context
+      );
+    }
 
-  /**
-   * @covers \Papaya\Message\Manager::log
-   */
-  public function testLogWithContext() {
-    $message = new Log(
-      \Papaya\Message::SEVERITY_INFO, Logable::GROUP_COMMUNITY, 'TEST'
-    );
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Context\Data $context */
-    $context = $this->createMock(Context\Data::class);
-    $message->context()->append($context);
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
-    $dispatcher = $this->createMock(Dispatcher::class);
-    $dispatcher
-      ->expects($this->once())
-      ->method('dispatch')
-      ->with($message);
-    $manager = new Manager();
-    $manager->addDispatcher($dispatcher);
-    $manager->log(
-      \Papaya\Message::SEVERITY_INFO,
-      Logable::GROUP_COMMUNITY,
-      'TEST',
-      $context
-    );
-  }
+    public function testLogWithData() {
+      $message = new Log(
+        \Papaya\Message::SEVERITY_INFO, Logable::GROUP_COMMUNITY, 'TEST'
+      );
+      $message->context()->append(new Context\Variable('data'));
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
+      $dispatcher = $this->createMock(Dispatcher::class);
+      $dispatcher
+        ->expects($this->once())
+        ->method('dispatch')
+        ->with($message);
+      $manager = new Manager();
+      $manager->addDispatcher($dispatcher);
+      $manager->log(
+        \Papaya\Message::SEVERITY_INFO,
+        Logable::GROUP_COMMUNITY,
+        'TEST',
+        'data'
+      );
+    }
 
-  /**
-   * @covers \Papaya\Message\Manager::log
-   */
-  public function testLogWithContextGroup() {
-    $message = new Log(
-      \Papaya\Message::SEVERITY_INFO, Logable::GROUP_COMMUNITY, 'TEST'
-    );
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Context\Data $context */
-    $context = $this->createMock(Context\Data::class);
-    $message->context()->append($context);
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
-    $dispatcher = $this->createMock(Dispatcher::class);
-    $dispatcher
-      ->expects($this->once())
-      ->method('dispatch')
-      ->with($message);
-    $manager = new Manager();
-    $manager->addDispatcher($dispatcher);
-    $manager->log(
-      \Papaya\Message::SEVERITY_INFO,
-      Logable::GROUP_COMMUNITY,
-      'TEST',
-      $context
-    );
-  }
+    public function testEncapsulate() {
+      $manager = new Manager();
+      $manager->papaya($papaya = $this->mockPapaya()->application());
+      $sandbox = $manager->encapsulate('substr');
+      $this->assertInternalType('callable', $sandbox);
+      /** @noinspection PhpUndefinedMethodInspection */
+      $this->assertSame($papaya, $sandbox[0]->papaya());
+    }
 
-  /**
-   * @covers \Papaya\Message\Manager::log
-   */
-  public function testLogWithData() {
-    $message = new Log(
-      \Papaya\Message::SEVERITY_INFO, Logable::GROUP_COMMUNITY, 'TEST'
-    );
-    $message->context()->append(new Context\Variable('data'));
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
-    $dispatcher = $this->createMock(Dispatcher::class);
-    $dispatcher
-      ->expects($this->once())
-      ->method('dispatch')
-      ->with($message);
-    $manager = new Manager();
-    $manager->addDispatcher($dispatcher);
-    $manager->log(
-      \Papaya\Message::SEVERITY_INFO,
-      Logable::GROUP_COMMUNITY,
-      'TEST',
-      'data'
-    );
-  }
+    public function testHooksSettingHooks() {
+      $hookOne = $this->createMock(Hook::class);
+      $hookTwo = $this->createMock(Hook::class);
+      $manager = new Manager();
+      $manager->hooks(
+        [$hookOne, $hookTwo]
+      );
+      $this->assertAttributeSame(
+        [$hookOne, $hookTwo],
+        '_hooks',
+        $manager
+      );
+    }
 
-  /**
-   * @covers \Papaya\Message\Manager::encapsulate
-   */
-  public function testEncapsulate() {
-    $manager = new Manager();
-    $manager->papaya($papaya = $this->mockPapaya()->application());
-    $sandbox = $manager->encapsulate('substr');
-    $this->assertInternalType('callable', $sandbox);
-    /** @noinspection PhpUndefinedMethodInspection */
-    $this->assertSame($papaya, $sandbox[0]->papaya());
-  }
+    public function testHooksReadHooks() {
+      $hookOne = $this->createMock(Hook::class);
+      $manager = new Manager();
+      $manager->hooks([$hookOne]);
+      $this->assertSame(
+        [$hookOne],
+        $manager->hooks()
+      );
+    }
 
-  /**
-   * @covers \Papaya\Message\Manager::hooks
-   */
-  public function testHooksSettingHooks() {
-    $hookOne = $this->createMock(Hook::class);
-    $hookTwo = $this->createMock(Hook::class);
-    $manager = new Manager();
-    $manager->hooks(
-      array($hookOne, $hookTwo)
-    );
-    $this->assertAttributeSame(
-      array($hookOne, $hookTwo),
-      '_hooks',
-      $manager
-    );
-  }
+    public function testHooksReadHooksImplizitCreate() {
+      $manager = new Manager();
+      $this->assertCount(2, $manager->hooks());
+    }
 
-  /**
-   * @covers \Papaya\Message\Manager::hooks
-   */
-  public function testHooksReadHooks() {
-    $hookOne = $this->createMock(Hook::class);
-    $manager = new Manager();
-    $manager->hooks(array($hookOne));
-    $this->assertSame(
-      array($hookOne),
-      $manager->hooks()
-    );
-  }
+    public function testSetUp() {
+      $errorReporting = error_reporting();
+      $options = $this->mockPapaya()->options();
+      $hookOne = $this->createMock(Hook::class);
+      $hookOne
+        ->expects($this->once())
+        ->method('activate');
+      $hookTwo = $this->createMock(Hook::class);
+      $hookTwo
+        ->expects($this->once())
+        ->method('activate');
 
-  /**
-   * @covers \Papaya\Message\Manager::hooks
-   */
-  public function testHooksReadHooksImplizitCreate() {
-    $manager = new Manager();
-    $this->assertCount(2, $manager->hooks());
-  }
+      $manager = new Manager();
+      $manager->hooks([$hookOne, $hookTwo]);
+      $manager->setUp($options);
 
-  /**
-   * @covers \Papaya\Message\Manager::setUp
-   */
-  public function testSetUp() {
-    $errorReporting = error_reporting();
-    $options = $this->mockPapaya()->options();
-    $hookOne = $this->createMock(Hook::class);
-    $hookOne
-      ->expects($this->once())
-      ->method('activate');
-    $hookTwo = $this->createMock(Hook::class);
-    $hookTwo
-      ->expects($this->once())
-      ->method('activate');
+      $this->assertAttributeGreaterThan(
+        0, '_startTime', Context\Runtime::class
+      );
+      $this->assertEquals(E_ALL & ~E_STRICT, error_reporting());
 
-    $manager = new Manager();
-    $manager->hooks(array($hookOne, $hookTwo));
-    $manager->setUp($options);
+      error_reporting($errorReporting);
+    }
 
-    $this->assertAttributeGreaterThan(
-      0, '_startTime', Context\Runtime::class
-    );
-    $this->assertEquals(E_ALL & ~E_STRICT, error_reporting());
+    public function testSetUpWithTemplate() {
+      $errorReporting = error_reporting();
 
-    error_reporting($errorReporting);
-  }
+      $template = $this->createMock(Template::class);
+      $options = $this->mockPapaya()->options();
+      $manager = new Manager();
+      $manager->setUp($options, $template);
 
+      $this->assertSame($template, $manager->getTemplate());
 
-  /**
-   * @covers \Papaya\Message\Manager::debug
-   */
-  public function testDebug() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
-    $dispatcher = $this->createMock(Dispatcher::class);
-    $dispatcher
-      ->expects($this->once())
-      ->method('dispatch')
-      ->with($this->isInstanceOf(Log::class));
-    $manager = new Manager();
-    $manager->addDispatcher($dispatcher);
-    $manager->debug('test');
+      error_reporting($errorReporting);
+    }
+
+    public function testDebug() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Dispatcher $dispatcher */
+      $dispatcher = $this->createMock(Dispatcher::class);
+      $dispatcher
+        ->expects($this->once())
+        ->method('dispatch')
+        ->with($this->isInstanceOf(Log::class));
+      $manager = new Manager();
+      $manager->addDispatcher($dispatcher);
+      $manager->debug('test');
+    }
   }
 }
