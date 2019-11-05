2d61d4347c379921a0a8cbf1c8e83a3118871c24
Thomas Weinert <thomas@weinert.info>
2019-11-05 14:12:35 +0100

    [CLEANUP] Reformat Iterator\Tree\DepthLimit

--- src/system/Papaya/Iterator/Tree/DepthLimit.php
+++ src/system/Papaya/Iterator/Tree/DepthLimit.php
@@ -12,141 +12,139 @@
  *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  *  FOR A PARTICULAR PURPOSE.
  */
-namespace Papaya\Iterator\Tree;
-
-use Papaya\Iterator\RecursiveTraversableIterator;
-
-/**
- * An iterator that limit the recursion to a specified maximum depth
- *
- * @package Papaya-Library
- * @subpackage Iterator
- */
-class DepthLimit implements \OuterIterator, \RecursiveIterator {
-
-  private $_maximumDepth;
+namespace Papaya\Iterator\Tree {
 
   /**
-   * @var \Iterator
+   * An iterator that limit the recursion to a specified maximum depth
+   *
+   * @package Papaya-Library
+   * @subpackage Iterator
    */
-  private $_iterator;
+  class DepthLimit implements \OuterIterator, \RecursiveIterator {
 
-  /**
-   * @var Items
-   */
-  private $_innerIterator;
-  /**
-   * @var int
-   */
-  private $_offset;
+    private $_maximumDepth;
 
-  /**
-   * @param \RecursiveIterator $iterator
-   * @param int $maximumDepth
-   * @param int $offset
-   */
-  public function __construct(\RecursiveIterator $iterator, $maximumDepth, $offset = 0) {
-    $this->_iterator = $iterator;
-    $this->_maximumDepth = (int)$maximumDepth;
-    $this->_offset = (int)$offset;
-  }
+    /**
+     * @var \Iterator
+     */
+    private $_iterator;
 
-  /**
-   * return Iterator
-   */
-  public function getInnerIterator() {
-    if (NULL === $this->_innerIterator) {
-      if ($this->_offset < 1) {
-        $this->_innerIterator = $this->_iterator;
-      } else {
-        $offsetItemsIterator = new \RecursiveIteratorIterator(
-          $this->_iterator, \RecursiveIteratorIterator::SELF_FIRST
-        );
-        $offsetItemsIterator->setMaxDepth($this->_offset);
-        $items = [];
-        $children = [];
-        foreach ($offsetItemsIterator as $key => $value) {
-          if ($this->_offset === $offsetItemsIterator->getDepth()) {
-            $items[$key] = $value;
-            if ($offsetItemsIterator->callHasChildren()) {
-              $children[$key] =  $offsetItemsIterator->callGetChildren();
+    /**
+     * @var Items
+     */
+    private $_innerIterator;
+    /**
+     * @var int
+     */
+    private $_offset;
+
+    /**
+     * @param \RecursiveIterator $iterator
+     * @param int $maximumDepth
+     * @param int $offset
+     */
+    public function __construct(\RecursiveIterator $iterator, $maximumDepth, $offset = 0) {
+      $this->_iterator = $iterator;
+      $this->_maximumDepth = (int)$maximumDepth;
+      $this->_offset = (int)$offset;
+    }
+
+    /**
+     * return Iterator
+     */
+    public function getInnerIterator() {
+      if (NULL === $this->_innerIterator) {
+        if ($this->_offset < 1) {
+          $this->_innerIterator = $this->_iterator;
+        } else {
+          $offsetItemsIterator = new \RecursiveIteratorIterator(
+            $this->_iterator, \RecursiveIteratorIterator::SELF_FIRST
+          );
+          $offsetItemsIterator->setMaxDepth($this->_offset);
+          $items = [];
+          $children = [];
+          foreach ($offsetItemsIterator as $key => $value) {
+            if ($this->_offset === $offsetItemsIterator->getDepth()) {
+              $items[$key] = $value;
+              if ($offsetItemsIterator->callHasChildren()) {
+                $children[$key] = $offsetItemsIterator->callGetChildren();
+              }
             }
           }
-        }
-        $this->_innerIterator = new Items(new \ArrayIterator($items), Items::ATTACH_TO_KEYS);
-        foreach ($children as $key => $itemChildren) {
-          $this->_innerIterator->attachItemIterator($key, $itemChildren);
+          $this->_innerIterator = new Items(new \ArrayIterator($items), Items::ATTACH_TO_KEYS);
+          foreach ($children as $key => $itemChildren) {
+            $this->_innerIterator->attachItemIterator($key, $itemChildren);
+          }
         }
       }
+      return $this->_innerIterator;
     }
-    return $this->_innerIterator;
-  }
 
-  /**
-   * Return TRUE if here are children on the inner iterator and the maximum level is larger 1.
-   *
-   * @see \RecursiveIterator::hasChildren()
-   *
-   * @return bool
-   */
-  public function hasChildren() {
-    return $this->_maximumDepth > 1 && $this->getInnerIterator()->hasChildren();
-  }
-
-  /**
-   * Return an RecursiveIterator for the attached Traversable of the current target. If
-   * The attached Traversable is an RecursiveIterator it will be returned. If it is not
-   * The method creates a new Instance of this class for the Traversable.   *
-   *
-   * @see \RecursiveIterator::hasChildren()
-   *
-   * @return \RecursiveIterator
-   */
-  public function getChildren() {
-    if ($this->hasChildren()) {
-      return new self($this->getInnerIterator()->getChildren(), $this->_maximumDepth - 1, $this->_offset - 1);
+    /**
+     * Return TRUE if here are children on the inner iterator and the maximum level is larger 1.
+     *
+     * @return bool
+     * @see \RecursiveIterator::hasChildren()
+     *
+     */
+    public function hasChildren() {
+      return $this->_maximumDepth > 1 && $this->getInnerIterator()->hasChildren();
     }
-    return new \RecursiveArrayIterator([]);
-  }
 
-  /**
-   * Rewind iterator to first element
-   */
-  public function rewind() {
-    $this->getInnerIterator()->rewind();
-  }
+    /**
+     * Return an RecursiveIterator for the attached Traversable of the current target. If
+     * The attached Traversable is an RecursiveIterator it will be returned. If it is not
+     * The method creates a new Instance of this class for the Traversable.   *
+     *
+     * @return \RecursiveIterator
+     * @see \RecursiveIterator::hasChildren()
+     *
+     */
+    public function getChildren() {
+      return $this->hasChildren()
+        ? new self($this->getInnerIterator()->getChildren(), $this->_maximumDepth - 1, $this->_offset - 1)
+        : new \RecursiveArrayIterator([]);
+    }
 
-  /**
-   * Fetch next element
-   */
-  public function next() {
-    $this->getInnerIterator()->next();
-  }
+    /**
+     * Rewind iterator to first element
+     */
+    public function rewind() {
+      $this->getInnerIterator()->rewind();
+    }
 
-  /**
-   * Return current key
-   *
-   * @return mixed
-   */
-  public function key() {
-    return $this->getInnerIterator()->key();
-  }
+    /**
+     * Fetch next element
+     */
+    public function next() {
+      $this->getInnerIterator()->next();
+    }
 
-  /**
-   * Return current element
-   *
-   * @return mixed
-   */
-  public function current() {
-    return $this->getInnerIterator()->current();
-  }
+    /**
+     * Return current key
+     *
+     * @return mixed
+     */
+    public function key() {
+      return $this->getInnerIterator()->key();
+    }
 
-  /**
-   * Valid if the current element is valid.
-   *
-   * @return bool
-   */
-  public function valid() {
-    return $this->getInnerIterator()->valid();
+    /**
+     * Return current element
+     *
+     * @return mixed
+     */
+    public function current() {
+      return $this->getInnerIterator()->current();
+    }
+
+    /**
+     * Valid if the current element is valid.
+     *
+     * @return bool
+     */
+    public function valid() {
+      return $this->getInnerIterator()->valid();
+    }
   }
 }
