b797e3307a4055d9b74e63bb5ae15bcd392809a8
Thomas Weinert <thomas@weinert.info>
2019-11-05 15:26:26 +0100

    [TEST] Add Tests For Iterator\RecursiveTraversableIterator

--- tests/system/Papaya/Iterator/RecursiveTraversableIteratorTest.php
+++ tests/system/Papaya/Iterator/RecursiveTraversableIteratorTest.php
@@ -13,47 +13,123 @@
  *  FOR A PARTICULAR PURPOSE.
  */
 
-namespace Papaya\Iterator;
+namespace Papaya\Iterator {
 
-class RecursiveTraversableIteratorTest extends \PHPUnit_Framework_TestCase {
-
-  /**
-   * @covers \Papaya\Iterator\RecursiveTraversableIterator
-   */
-  public function testRecursiveTraversableIteratorForArray() {
-    $iterator = new RecursiveTraversableIterator([0, 1, [2, 3]], RecursiveTraversableIterator::LEAVES_ONLY);
-    $this->assertEquals(
-      [0,1,2,3],
-      iterator_to_array($iterator, FALSE)
-    );
-  }
+  use UnexpectedValueException;
 
   /**
    * @covers \Papaya\Iterator\RecursiveTraversableIterator
    */
-  public function testRecursiveTraversableIteratorForRecursiveIterator() {
-    $iterator = new RecursiveTraversableIterator(new \RecursiveArrayIterator([0, 1, [2, 3]]), RecursiveTraversableIterator::LEAVES_ONLY);
-    $this->assertEquals(
-      [0,1,2,3],
-      iterator_to_array($iterator, FALSE)
-    );
+  class RecursiveTraversableIteratorTest extends \PHPUnit_Framework_TestCase {
+
+    public function testRecursiveTraversableIteratorForArray() {
+      $iterator = new RecursiveTraversableIterator([0, 1, [2, 3]], RecursiveTraversableIterator::LEAVES_ONLY);
+      $this->assertEquals(
+        [0, 1, 2, 3],
+        iterator_to_array($iterator, FALSE)
+      );
+    }
+
+    public function testRecursiveTraversableIteratorForInvalidTraversableExpectingException() {
+      $this->expectException(UnexpectedValueException::class);
+      $this->expectExceptionMessage('Unexpected value type: Expected "array, Traversable" but "NULL" given.');
+      new RecursiveTraversableIterator(NULL, RecursiveTraversableIterator::LEAVES_ONLY);
+    }
+
+    public function testRecursiveTraversableIteratorForNonRecursiveTraversableExpectingException() {
+      $iterator = new RecursiveTraversableIterator(new \ArrayIterator([]), RecursiveTraversableIterator::LEAVES_ONLY);
+      $this->expectException(UnexpectedValueException::class);
+      $this->expectExceptionMessage('Could not get RecursiveIterator for/from provided ArrayIterator.');
+        iterator_to_array($iterator, FALSE);
+    }
+
+    public function testRecursiveTraversableIteratorForRecursiveIterator() {
+      $iterator = new RecursiveTraversableIterator(
+        new \RecursiveArrayIterator([0, 1, [2, 3]]), RecursiveTraversableIterator::LEAVES_ONLY
+      );
+      $this->assertEquals(
+        [0, 1, 2, 3],
+        iterator_to_array($iterator, FALSE)
+      );
+    }
+
+    public function testRecursiveTraversableIteratorForIteratorAggregate() {
+      $iterator = new RecursiveTraversableIterator(
+        new RecursiveIteratorAggregate_TestStub(), RecursiveTraversableIterator::LEAVES_ONLY
+      );
+      $this->assertEquals(
+        [0, 1, 2, 3],
+        iterator_to_array($iterator, FALSE)
+      );
+    }
+
+    public function testRecursiveTraversableIteratorForArraySelfFirst() {
+      $iterator = new RecursiveTraversableIterator([0, 1, [2, 3]], RecursiveTraversableIterator::SELF_FIRST);
+      $this->assertEquals(
+        [0, 1, [2, 3], 2, 3],
+        iterator_to_array($iterator, FALSE)
+      );
+    }
+
+    public function testRecursiveTraversableIteratorForArrayChildFirst() {
+      $iterator = new RecursiveTraversableIterator([0, 1, [2, 3]], RecursiveTraversableIterator::CHILD_FIRST);
+      $this->assertEquals(
+        [0, 1, 2, 3, [2, 3]],
+        iterator_to_array($iterator, FALSE)
+      );
+    }
+
+    public function testGetDepthDelegation() {
+      $iterator = new RecursiveTraversableIterator([0, 1, [2, 3]], RecursiveTraversableIterator::LEAVES_ONLY);
+      $result = [];
+      foreach ($iterator as $value) {
+        $result[] = ['value' => $value, 'depth' => $iterator->getDepth()];
+      }
+      $this->assertSame(
+        [
+          ['value' => 0, 'depth' => 0],
+          ['value' => 1, 'depth' => 0],
+          ['value' => 2, 'depth' => 1],
+          ['value' => 3, 'depth' => 1],
+        ],
+        $result
+      );
+    }
+
+    public function testGetMaxDepthAfterSet() {
+      $iterator = new RecursiveTraversableIterator([0, 1, [2, 3]], RecursiveTraversableIterator::LEAVES_ONLY);
+      $iterator->setMaxDepth(4);
+      $this->assertSame(
+        4,
+        $iterator->getMaxDepth()
+      );
+    }
+
+    public function testGetSubIteratorDelegation() {
+      $iterator = new RecursiveTraversableIterator([0, 1, [2, 3]], RecursiveTraversableIterator::LEAVES_ONLY);
+      $result = [];
+      foreach ($iterator as $value) {
+        $result[] = [
+          'value' => $value,
+          'l0' => ($iterator->getSubIterator(0)) instanceof \RecursiveIterator,
+          'l1' => ($iterator->getSubIterator(1)) instanceof \RecursiveIterator
+        ];
+      }
+      $this->assertSame(
+        [
+          ['value' => 0, 'l0' => TRUE, 'l1' => FALSE],
+          ['value' => 1, 'l0' => TRUE, 'l1' => FALSE],
+          ['value' => 2, 'l0' => TRUE, 'l1' => TRUE],
+          ['value' => 3, 'l0' => TRUE, 'l1' => TRUE],
+        ],
+        $result
+      );
+    }
   }
 
-  /**
-   * @covers \Papaya\Iterator\RecursiveTraversableIterator
-   */
-  public function testRecursiveTraversableIteratorForIteratorAggregate() {
-    $iterator = new RecursiveTraversableIterator(new RecursiveIteratorAggregate_TestStub(), RecursiveTraversableIterator::LEAVES_ONLY);
-    $this->assertEquals(
-      [0,1,2,3],
-      iterator_to_array($iterator, FALSE)
-    );
-  }
-
-}
-
-class RecursiveIteratorAggregate_TestStub implements \IteratorAggregate {
-  public function getIterator() {
-    return new \RecursiveArrayIterator([0, 1, [2, 3]]);
+  class RecursiveIteratorAggregate_TestStub implements \IteratorAggregate {
+    public function getIterator() {
+      return new \RecursiveArrayIterator([0, 1, [2, 3]]);
+    }
   }
 }
