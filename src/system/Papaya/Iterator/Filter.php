57c658c78f214a52d747b77bcc99ce2176675a35
Thomas Weinert <thomas@weinert.info>
2019-11-05 14:22:00 +0100

    [TEST] Add Tests For Iterator\Filter

--- src/system/Papaya/Iterator/Filter.php
+++ src/system/Papaya/Iterator/Filter.php
@@ -12,107 +12,111 @@
  *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  *  FOR A PARTICULAR PURPOSE.
  */
-namespace Papaya\Iterator;
+namespace Papaya\Iterator {
 
-use Papaya\Filter as InputFilter;
-use Papaya\Utility;
-
-/**
- * An filter iterator to filter an given iterator using a papaya filter object.
- *
- * @package Papaya-Library
- * @subpackage Iterator
- */
-class Filter extends \FilterIterator {
-  const FILTER_VALUES = 1;
-
-  const FILTER_KEYS = 2;
-
-  const FILTER_BOTH = 3;
+  use Papaya\Filter as InputFilter;
+  use Papaya\Utility;
 
   /**
-   * @var Filter
-   */
-  private $_filter;
-
-  /**
-   * @var int
-   */
-  private $_target;
-
-  /**
-   * Create object and store iterator, pattern, flags and offset.
+   * An filter iterator to filter an given iterator using a papaya filter object.
    *
-   * @param \Traversable $traversable
-   * @param InputFilter $filter
-   * @param int $target
+   * @package Papaya-Library
+   * @subpackage Iterator
    */
-  public function __construct(
-    \Traversable $traversable, InputFilter $filter, $target = self::FILTER_VALUES
-  ) {
-    parent::__construct(
-      $traversable instanceof \Iterator ? $traversable : new TraversableIterator($traversable)
-    );
-    $this->_filter = $filter;
-    $this->_target = (int)$target;
-  }
+  class Filter extends \FilterIterator {
 
-  /**
-   * Validate the current item and/or key using the filter object.
-   *
-   * @return bool
-   */
-  public function accept() {
-    if (
-      Utility\Bitwise::inBitmask(self::FILTER_VALUES, $this->_target) &&
-      !$this->isMatch($this->getInnerIterator()->current())) {
-      return FALSE;
+    const FILTER_VALUES = 1;
+
+    const FILTER_KEYS = 2;
+
+    const FILTER_BOTH = 3;
+
+    /**
+     * @var Filter
+     */
+    private $_filter;
+
+    /**
+     * @var int
+     */
+    private $_target;
+
+    /**
+     * Create object and store iterator, pattern, flags and offset.
+     *
+     * @param \Traversable $traversable
+     * @param InputFilter $filter
+     * @param int $target
+     */
+    public function __construct(
+      \Traversable $traversable, InputFilter $filter, $target = self::FILTER_VALUES
+    ) {
+      parent::__construct(
+        $traversable instanceof \Iterator ? $traversable : new TraversableIterator($traversable)
+      );
+      $this->_filter = $filter;
+      $this->_target = (int)$target;
     }
-    if (
-      Utility\Bitwise::inBitmask(self::FILTER_KEYS, $this->_target) &&
-      !$this->isMatch($this->getInnerIterator()->key())) {
-      return FALSE;
-    }
-    return TRUE;
-  }
 
-  /**
-   * Optionally filter the current value before returning it
-   *
-   * @return mixed
-   */
-  public function current() {
-    if (Utility\Bitwise::inBitmask(self::FILTER_VALUES, $this->_target)) {
-      return $this->_filter->filter(parent::current());
-    }
-    return parent::current();
-  }
-
-  /**
-   * Optionally filter the key value before returning it
-   *
-   * @return mixed
-   */
-  public function key() {
-    if (Utility\Bitwise::inBitmask(self::FILTER_KEYS, $this->_target)) {
-      return $this->_filter->filter(parent::key());
-    }
-    return parent::current();
-  }
-
-  /**
-   * Match pattern against a value (key or current). The value will be casted to string
-   *
-   * @param mixed $value
-   *
-   * @return bool
-   */
-  private function isMatch($value) {
-    try {
-      $this->_filter->validate($value);
+    /**
+     * Validate the current item and/or key using the filter object.
+     *
+     * @return bool
+     */
+    public function accept() {
+      if (
+        Utility\Bitwise::inBitmask(self::FILTER_VALUES, $this->_target) &&
+        !$this->isMatch($this->getInnerIterator()->current())
+      ) {
+        return FALSE;
+      }
+      if (
+        Utility\Bitwise::inBitmask(self::FILTER_KEYS, $this->_target) &&
+        !$this->isMatch($this->getInnerIterator()->key())
+      ) {
+        return FALSE;
+      }
       return TRUE;
-    } catch (InputFilter\Exception $e) {
     }
-    return FALSE;
+
+    /**
+     * Optionally filter the current value before returning it
+     *
+     * @return mixed
+     */
+    public function current() {
+      if (Utility\Bitwise::inBitmask(self::FILTER_VALUES, $this->_target)) {
+        return $this->_filter->filter(parent::current());
+      }
+      return parent::current();
+    }
+
+    /**
+     * Optionally filter the key value before returning it
+     *
+     * @return mixed
+     */
+    public function key() {
+      if (Utility\Bitwise::inBitmask(self::FILTER_KEYS, $this->_target)) {
+        return $this->_filter->filter(parent::key());
+      }
+      return parent::key();
+    }
+
+    /**
+     * Match pattern against a value (key or current). The value will be casted to string
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    private function isMatch($value) {
+      try {
+        $this->_filter->validate($value);
+        return TRUE;
+      } catch (InputFilter\Exception $e) {
+      }
+      return FALSE;
+    }
   }
 }
---
+++ tests/system/Papaya/Iterator/FilterTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * papaya CMS
+ *
+ * @copyright 2000-2019 by papayaCMS project - All rights reserved.
+ * @link http://www.papaya-cms.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ *  You can redistribute and/or modify this script under the terms of the GNU General Public
+ *  License (GPL) version 2, provided that the copyright and license notes, including these
+ *  lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.
+ */
+
+namespace Papaya\Iterator {
+
+  use Papaya\Filter\IntegerValue;
+  use Papaya\Test\TestCase;
+
+  /**
+   * @covers \Papaya\Iterator\Filter
+   */
+  class FilterTest extends TestCase {
+
+    public function testFilterForIntegerValues() {
+      $data = ['foo', 21, '42'];
+      $iterator = new Filter(new \ArrayIterator($data), new IntegerValue());
+      $this->assertSame(
+        [1 => 21, 2 => 42],
+        iterator_to_array($iterator)
+      );
+    }
+
+    public function testFilterForIntegerKeys() {
+      $data = ['foo' => 1, 21 => 2, '42' => 3];
+      $iterator = new Filter(new \ArrayIterator($data), new IntegerValue(), Filter::FILTER_KEYS);
+      $this->assertSame(
+        [21 => 2, 42 => 3],
+        iterator_to_array($iterator)
+      );
+    }
+
+    public function testFilterForInteger() {
+      $data = ['foo' => 1, 21 => 2, '42' => 3, 1 => 'foo'];
+      $iterator = new Filter(new \ArrayIterator($data), new IntegerValue(), Filter::FILTER_BOTH);
+      $this->assertSame(
+        [21 => 2, 42 => 3],
+        iterator_to_array($iterator)
+      );
+    }
+  }
+}
