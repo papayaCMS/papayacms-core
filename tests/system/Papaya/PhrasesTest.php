bef5ef1c8db35892cbc91f653a40271a9e7048f6
Thomas Weinert <thomas@weinert.info>
2019-11-06 12:14:43 +0100

    [TEST] Add Test For Phrases::getTextFmt()

--- tests/system/Papaya/PhrasesTest.php
+++ tests/system/Papaya/PhrasesTest.php
@@ -13,152 +13,153 @@
  *  FOR A PARTICULAR PURPOSE.
  */
 
-namespace Papaya;
-require_once __DIR__.'/../../bootstrap.php';
-
-class PhrasesTest extends \Papaya\TestCase {
+namespace Papaya {
+  require_once __DIR__.'/../../bootstrap.php';
 
   /**
    * @covers \Papaya\Phrases
    */
-  public function testConstructor() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
-    $storage = $this->createMock(Phrases\Storage::class);
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
-    $language = $this->createMock(Content\Language::class);
-    $phrases = new Phrases($storage, $language);
-    $this->assertSame($storage, $phrases->getStorage());
-    $this->assertSame($language, $phrases->getLanguage());
-  }
+  class PhrasesTest extends TestCase {
 
-  /**
-   * @covers \Papaya\Phrases
-   */
-  public function testGetGroupsAfterSet() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
-    $storage = $this->createMock(Phrases\Storage::class);
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
-    $language = $this->createMock(Content\Language::class);
-    $phrases = new Phrases($storage, $language);
-    $groups = $this
-      ->getMockBuilder(Phrases\Groups::class)
-      ->disableOriginalConstructor()
-      ->getMock();
-    $phrases->groups = $groups;
-    $this->assertSame($groups, $phrases->groups);
-  }
+    public function testConstructor() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
+      $storage = $this->createMock(Phrases\Storage::class);
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
+      $language = $this->createMock(Content\Language::class);
+      $phrases = new Phrases($storage, $language);
+      $this->assertSame($storage, $phrases->getStorage());
+      $this->assertSame($language, $phrases->getLanguage());
+    }
 
-  /**
-   * @covers \Papaya\Phrases
-   */
-  public function testGetGroupsImplicitCreate() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
-    $storage = $this->createMock(Phrases\Storage::class);
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
-    $language = $this->createMock(Content\Language::class);
-    $phrases = new Phrases($storage, $language);
-    $this->assertInstanceOf(Phrases\Groups::class, $phrases->groups);
-  }
+    public function testGetGroupsAfterSet() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
+      $storage = $this->createMock(Phrases\Storage::class);
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
+      $language = $this->createMock(Content\Language::class);
+      $phrases = new Phrases($storage, $language);
+      $groups = $this
+        ->getMockBuilder(Phrases\Groups::class)
+        ->disableOriginalConstructor()
+        ->getMock();
+      $phrases->groups = $groups;
+      $this->assertSame($groups, $phrases->groups);
+    }
 
-  /**
-   * @covers \Papaya\Phrases::defaultGroup
-   */
-  public function testDefaultGroupGetAfterSet() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
-    $storage = $this->createMock(Phrases\Storage::class);
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
-    $language = $this->createMock(Content\Language::class);
-    $phrases = new Phrases($storage, $language);
-    $phrases->defaultGroup('TestGroup');
-    $this->assertEquals('TestGroup', $phrases->defaultGroup());
-  }
+    public function testGetGroupsImplicitCreate() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
+      $storage = $this->createMock(Phrases\Storage::class);
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
+      $language = $this->createMock(Content\Language::class);
+      $phrases = new Phrases($storage, $language);
+      $this->assertInstanceOf(Phrases\Groups::class, $phrases->groups);
+    }
 
-  /**
-   * @covers \Papaya\Phrases::defaultGroup
-   */
-  public function testDefaultGroupImplicitInit() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
-    $storage = $this->createMock(Phrases\Storage::class);
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
-    $language = $this->createMock(Content\Language::class);
-    $phrases = new Phrases($storage, $language);
-    $phrases->papaya($this->mockPapaya()->application());
-    $this->assertEquals('test.html', $phrases->defaultGroup());
-  }
+    public function testDefaultGroupGetAfterSet() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
+      $storage = $this->createMock(Phrases\Storage::class);
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
+      $language = $this->createMock(Content\Language::class);
+      $phrases = new Phrases($storage, $language);
+      $phrases->defaultGroup('TestGroup');
+      $this->assertEquals('TestGroup', $phrases->defaultGroup());
+    }
 
-  /**
-   * @covers \Papaya\Phrases::get
-   */
-  public function testGetCreatesStringObject() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
-    $language = $this->createMock(Content\Language::class);
-    $language
-      ->expects($this->once())
-      ->method('__get')
-      ->with('id')
-      ->will($this->returnValue(1));
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
-    $storage = $this->createMock(Phrases\Storage::class);
-    $storage
-      ->expects($this->once())
-      ->method('get')
-      ->with('Some Phrase', 'TestGroup', 1)
-      ->will($this->returnValue('Success'));
-    $phrases = new Phrases($storage, $language);
-    $phrases->papaya($this->mockPapaya()->application());
-    $phrases->defaultGroup('TestGroup');
-    $phrase = $phrases->get('Some Phrase');
-    $this->assertInstanceOf(UI\Text::class, $phrase);
-    $this->assertEquals('Success', (string)$phrase);
-  }
+    public function testDefaultGroupImplicitInit() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
+      $storage = $this->createMock(Phrases\Storage::class);
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
+      $language = $this->createMock(Content\Language::class);
+      $phrases = new Phrases($storage, $language);
+      $phrases->papaya($this->mockPapaya()->application());
+      $this->assertEquals('test.html', $phrases->defaultGroup());
+    }
 
-  /**
-   * @covers \Papaya\Phrases::getList
-   */
-  public function testGetListCreatesListObject() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
-    $language = $this->createMock(Content\Language::class);
-    $language
-      ->expects($this->once())
-      ->method('__get')
-      ->with('id')
-      ->will($this->returnValue(1));
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
-    $storage = $this->createMock(Phrases\Storage::class);
-    $storage
-      ->expects($this->once())
-      ->method('get')
-      ->with('Some Phrase', 'TestGroup', 1)
-      ->will($this->returnValue('Success'));
-    $phrases = new Phrases($storage, $language);
-    $phrases->papaya($this->mockPapaya()->application());
-    $phrases->defaultGroup('TestGroup');
-    $list = iterator_to_array($phrases->getList(array('Some Phrase')));
-    $this->assertInstanceOf(UI\Text::class, $list[0]);
-    $this->assertEquals('Success', (string)$list[0]);
-  }
+    public function testGetCreatesStringObject() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
+      $language = $this->createMock(Content\Language::class);
+      $language
+        ->expects($this->once())
+        ->method('__get')
+        ->with('id')
+        ->willReturn(1);
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
+      $storage = $this->createMock(Phrases\Storage::class);
+      $storage
+        ->expects($this->once())
+        ->method('get')
+        ->with('Some Phrase', 'TestGroup', 1)
+        ->willReturn('Success');
+      $phrases = new Phrases($storage, $language);
+      $phrases->papaya($this->mockPapaya()->application());
+      $phrases->defaultGroup('TestGroup');
+      $phrase = $phrases->get('Some Phrase');
+      $this->assertInstanceOf(UI\Text::class, $phrase);
+      $this->assertEquals('Success', (string)$phrase);
+    }
 
-  /**
-   * @covers \Papaya\Phrases::getList
-   */
-  public function testGetText() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
-    $language = $this->createMock(Content\Language::class);
-    $language
-      ->expects($this->once())
-      ->method('__get')
-      ->with('id')
-      ->will($this->returnValue(1));
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
-    $storage = $this->createMock(Phrases\Storage::class);
-    $storage
-      ->expects($this->once())
-      ->method('get')
-      ->with('Some Phrase', 'TestGroup', 1)
-      ->will($this->returnValue('Success'));
-    $phrases = new Phrases($storage, $language);
-    $phrases->papaya($this->mockPapaya()->application());
-    $phrases->defaultGroup('TestGroup');
-    $this->assertEquals('Success', $phrases->getText('Some Phrase'));
+    public function testGetListCreatesListObject() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
+      $language = $this->createMock(Content\Language::class);
+      $language
+        ->expects($this->once())
+        ->method('__get')
+        ->with('id')
+        ->willReturn(1);
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
+      $storage = $this->createMock(Phrases\Storage::class);
+      $storage
+        ->expects($this->once())
+        ->method('get')
+        ->with('Some Phrase', 'TestGroup', 1)
+        ->willReturn('Success');
+      $phrases = new Phrases($storage, $language);
+      $phrases->papaya($this->mockPapaya()->application());
+      $phrases->defaultGroup('TestGroup');
+      $list = iterator_to_array($phrases->getList(['Some Phrase']));
+      $this->assertInstanceOf(UI\Text::class, $list[0]);
+      $this->assertEquals('Success', (string)$list[0]);
+    }
+
+    public function testGetText() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
+      $language = $this->createMock(Content\Language::class);
+      $language
+        ->expects($this->once())
+        ->method('__get')
+        ->with('id')
+        ->willReturn(1);
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
+      $storage = $this->createMock(Phrases\Storage::class);
+      $storage
+        ->expects($this->once())
+        ->method('get')
+        ->with('Some Phrase', 'TestGroup', 1)
+        ->willReturn('Success');
+      $phrases = new Phrases($storage, $language);
+      $phrases->papaya($this->mockPapaya()->application());
+      $phrases->defaultGroup('TestGroup');
+      $this->assertEquals('Success', $phrases->getText('Some Phrase'));
+    }
+
+    public function testGetTextFmt() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Content\Language $language */
+      $language = $this->createMock(Content\Language::class);
+      $language
+        ->expects($this->once())
+        ->method('__get')
+        ->with('id')
+        ->willReturn(1);
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Phrases\Storage $storage */
+      $storage = $this->createMock(Phrases\Storage::class);
+      $storage
+        ->expects($this->once())
+        ->method('get')
+        ->with('Hello %s!', 'TestGroup', 1)
+        ->willReturn('Hello %s!');
+      $phrases = new Phrases($storage, $language);
+      $phrases->papaya($this->mockPapaya()->application());
+      $phrases->defaultGroup('TestGroup');
+      $this->assertEquals('Hello World!', $phrases->getTextFmt('Hello %s!', ['World']));
+    }
   }
 }
