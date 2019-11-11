825b8e66846071a50d34445300555d5d5f630a3f
Thomas Weinert <thomas@weinert.info>
2019-11-11 15:48:32 +0100

    [TEST] Add Tests For Media\File\Info

--- src/system/Papaya/Media/File/Info.php
+++ src/system/Papaya/Media/File/Info.php
@@ -12,118 +12,119 @@
  *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  *  FOR A PARTICULAR PURPOSE.
  */
-namespace Papaya\Media\File;
+namespace Papaya\Media\File {
 
-use Papaya\Application;
+  use Papaya\Application;
 
-abstract class Info implements Application\Access, \ArrayAccess, \IteratorAggregate {
-  use Application\Access\Aggregation;
+  abstract class Info implements Application\Access, \ArrayAccess, \IteratorAggregate {
+    use Application\Access\Aggregation;
 
-  /**
-   * @var string
-   */
-  private $_file;
+    /**
+     * @var string
+     */
+    private $_file;
 
-  /**
-   * @var array
-   */
-  private $_properties;
+    /**
+     * @var array
+     */
+    private $_properties;
 
-  /**
-   * @var string
-   */
-  private $_originalFileName;
+    /**
+     * @var string
+     */
+    private $_originalFileName;
 
-  /**
-   * Info constructor.
-   *
-   * @param string $file
-   * @param string $originalFileName
-   */
-  public function __construct($file, $originalFileName = '') {
-    $this->_file = $file;
-    $this->_originalFileName = $originalFileName;
-  }
-
-  /**
-   * @return string
-   */
-  public function getFile() {
-    return $this->_file;
-  }
-
-  /**
-   * @return string
-   */
-  public function getOriginalFileName() {
-    return $this->_originalFileName;
-  }
-
-  /**
-   * @param array $fileProperties
-   * @return bool
-   */
-  public function isSupported(
-    /** @noinspection PhpUnusedParameterInspection */
-    array $fileProperties = []
-  ) {
-    return TRUE;
-  }
-
-  /**
-   * @return \Traversable
-   */
-  public function getIterator() {
-    return new \ArrayIterator($this->getProperties());
-  }
-
-  /**
-   * @return array
-   */
-  protected function fetchProperties() {
-    return [
-      'filesize' => \filesize($this->_file)
-    ];
-  }
-
-  /**
-   * @return array
-   */
-  private function getProperties() {
-    if (NULL === $this->_properties) {
-      $this->_properties = $this->fetchProperties();
+    /**
+     * Info constructor.
+     *
+     * @param string $file
+     * @param string $originalFileName
+     */
+    public function __construct($file, $originalFileName = '') {
+      $this->_file = $file;
+      $this->_originalFileName = $originalFileName;
     }
-    return $this->_properties;
-  }
 
-  /**
-   * @param string $offset
-   * @return bool
-   */
-  public function offsetExists($offset) {
-    return \array_key_exists($offset, $this->getProperties());
-  }
+    /**
+     * @return string
+     */
+    public function getFile() {
+      return $this->_file;
+    }
 
-  /**
-   * @param string $offset
-   * @return bool
-   */
-  public function offsetGet($offset) {
-    return $this->getProperties()[$offset];
-  }
+    /**
+     * @return string
+     */
+    public function getOriginalFileName() {
+      return $this->_originalFileName;
+    }
 
-  /**
-   * @param string $offset
-   * @param mixed $value
-   */
-  public function offsetSet($offset, $value) {
-    throw new \BadMethodCallException(\sprintf('Object %s is immutable.', static::class));
-  }
+    /**
+     * @param array $fileProperties
+     * @return bool
+     */
+    public function isSupported(
+      /** @noinspection PhpUnusedParameterInspection */
+      array $fileProperties = []
+    ) {
+      return TRUE;
+    }
 
-  /**
-   * @param string $offset
-   */
-  public function offsetUnset($offset) {
-    throw new \BadMethodCallException(\sprintf('Object %s is immutable.', static::class));
+    /**
+     * @return \Traversable
+     */
+    public function getIterator() {
+      return new \ArrayIterator($this->getProperties());
+    }
+
+    /**
+     * @return array
+     */
+    protected function fetchProperties() {
+      return [
+        'filesize' => \filesize($this->_file)
+      ];
+    }
+
+    /**
+     * @return array
+     */
+    private function getProperties() {
+      if (NULL === $this->_properties) {
+        $this->_properties = $this->fetchProperties();
+      }
+      return $this->_properties;
+    }
+
+    /**
+     * @param string $offset
+     * @return bool
+     */
+    public function offsetExists($offset) {
+      return \array_key_exists($offset, $this->getProperties());
+    }
+
+    /**
+     * @param string $offset
+     * @return bool
+     */
+    public function offsetGet($offset) {
+      return $this->getProperties()[$offset];
+    }
+
+    /**
+     * @param string $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value) {
+      throw new \BadMethodCallException(\sprintf('Object %s is immutable.', static::class));
+    }
+
+    /**
+     * @param string $offset
+     */
+    public function offsetUnset($offset) {
+      throw new \BadMethodCallException(\sprintf('Object %s is immutable.', static::class));
+    }
   }
 }
---
+++ tests/system/Papaya/Media/File/InfoTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * papaya CMS
+ *
+ * @copyright 2000-2019 by papayaCMS project - All rights reserved.
+ * @link http://www.papaya-cms.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ *  You can redistribute and/or modify this script under the terms of the GNU General Public
+ *  License (GPL) version 2, provided that the copyright and license notes, including these
+ *  lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.
+ */
+
+namespace Papaya\Media\File {
+
+  use BadMethodCallException;
+  use Papaya\Test\TestCase;
+
+  /**
+   * @covers \Papaya\Media\File\Info
+   */
+  class InfoTest extends TestCase {
+
+    public function testConstructor() {
+      $info = new Info_TestProxy(__DIR__.'/TestData/example.txt');
+      $this->assertSame(__DIR__.'/TestData/example.txt', $info->getFile());
+    }
+
+    public function testConstructorWithOriginalFileName() {
+      $info = new Info_TestProxy(__DIR__.'/TestData/example.txt', 'original.txt');
+      $this->assertSame(__DIR__.'/TestData/example.txt', $info->getFile());
+      $this->assertSame('original.txt', $info->getOriginalFileName());
+    }
+
+    public function testIsSupportedReturnsTrue() {
+      $info = new Info_TestProxy(__DIR__.'/TestData/example.txt');
+      $this->assertTrue($info->isSupported());
+    }
+
+    public function testGetIterator() {
+      $info = new Info_TestProxy(__DIR__.'/TestData/example.txt', 'original.txt');
+      $this->assertSame(['filesize' => 8], iterator_to_array($info));
+    }
+
+    public function testOffsetGet() {
+      $info = new Info_TestProxy(__DIR__.'/TestData/example.txt', 'original.txt');
+      $this->assertTrue(isset($info['filesize']));
+      $this->assertSame(8, $info['filesize']);
+    }
+
+    public function testOffsetSetExpectingException() {
+      $info = new Info_TestProxy(__DIR__.'/TestData/example.txt', 'original.txt');
+      $this->expectException(BadMethodCallException::class);
+      $this->expectExceptionMessage('Object Papaya\Media\File\Info_TestProxy is immutable.');
+      $info['filesize'] = 9;
+    }
+
+    public function testOffsetUnsetExpectingException() {
+      $info = new Info_TestProxy(__DIR__.'/TestData/example.txt', 'original.txt');
+      $this->expectException(BadMethodCallException::class);
+      $this->expectExceptionMessage('Object Papaya\Media\File\Info_TestProxy is immutable.');
+      unset($info['filesize']);
+    }
+  }
+
+  class Info_TestProxy extends Info {
+
+  }
+}
