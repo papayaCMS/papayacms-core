8bb5b3cce0ad1c54c564847bcf1870b7479fe547
Thomas Weinert <thomas@weinert.info>
2019-11-11 14:58:47 +0100

    [TEST] Add Tests For Media\File\Info\SVG

--- src/system/Papaya/Media/File/Info/SVG.php
+++ src/system/Papaya/Media/File/Info/SVG.php
@@ -14,59 +14,70 @@
  */
 
 /** @noinspection PhpComposerExtensionStubsInspection */
-namespace Papaya\Media\File\Info;
+namespace Papaya\Media\File\Info {
 
-class SVG extends \Papaya\Media\File\Info {
-  public $forceDOM = FALSE;
+  use Papaya\Media\File\Info;
+  use Papaya\XML\Document as XMLDocument;
 
-  const XMLNS_SVG = 'http://www.w3.org/2000/svg';
+  class SVG extends Info {
+    public $forceDOM = FALSE;
 
-  public function isSupported(array $fileProperties = []) {
-    $extension = \strrchr($this->getOriginalFileName(), '.');
-    return (
-      (
-        isset($fileProperties['mimetype']) &&
+    const XMLNS_SVG = 'http://www.w3.org/2000/svg';
+
+    /**
+     * @param array $fileProperties
+     * @return bool
+     */
+    public function isSupported(array $fileProperties = []) {
+      $extension = \strrchr($this->getOriginalFileName(), '.');
+      return (
         (
-          'image/svg' === $fileProperties['mimetype'] ||
-          'image/svg+xml' === $fileProperties['mimetype']
+          isset($fileProperties['mimetype']) &&
+          (
+            'image/svg' === $fileProperties['mimetype'] ||
+            'image/svg+xml' === $fileProperties['mimetype']
+          )
+        ) ||
+        (
+          '.svg' === $extension
         )
-      ) ||
-      (
-        '.svg' === $extension
-      )
-    );
-  }
-
-  protected function fetchProperties() {
-    $properties = [
-      'is_valid' => FALSE,
-      'width' => 0,
-      'height' => 0
-    ];
-    if (!$this->forceDOM && \class_exists('XMLReader')) {
-      $reader = new \XMLReader();
-      if (@$reader->open($this->getFile())) {
-        $found = @$reader->read();
-        while ($found && !('svg' === $reader->localName && self::XMLNS_SVG === $reader->namespaceURI)) {
-          $found = $reader->next('svg');
-        }
-        if ($found) {
-          $properties['is_valid'] = TRUE;
-          $properties['width'] = (int)$reader->getAttribute('width');
-          $properties['height'] = (int)$reader->getAttribute('height');
-        }
-      }
-    } else {
-      $document = new \Papaya\XML\Document();
-      if (@$document->load($this->getFile())) {
-        $node = $document->documentElement;
-        if ($node && 'svg' === $node->localName && self::XMLNS_SVG === $node->namespaceURI) {
-          $properties['is_valid'] = TRUE;
-          $properties['width'] = (int)$node->getAttribute('width');
-          $properties['height'] = (int)$node->getAttribute('height');
-        }
-      }
+      );
     }
-    return $properties;
+
+    /**
+     * @return array
+     */
+    protected function fetchProperties() {
+      $properties = [
+        'is_valid' => FALSE,
+        'width' => 0,
+        'height' => 0
+      ];
+      if (!$this->forceDOM && \class_exists('XMLReader')) {
+        $reader = new \XMLReader();
+        if (@$reader->open($this->getFile())) {
+          $found = @$reader->read();
+          while ($found && !('svg' === $reader->localName && self::XMLNS_SVG === $reader->namespaceURI)) {
+            $found = $reader->next('svg');
+          }
+          if ($found) {
+            $properties['is_valid'] = TRUE;
+            $properties['width'] = (int)$reader->getAttribute('width');
+            $properties['height'] = (int)$reader->getAttribute('height');
+          }
+        }
+      } else {
+        $document = new XMLDocument();
+        if (@$document->load($this->getFile())) {
+          $node = $document->documentElement;
+          if ($node && 'svg' === $node->localName && self::XMLNS_SVG === $node->namespaceURI) {
+            $properties['is_valid'] = TRUE;
+            $properties['width'] = (int)$node->getAttribute('width');
+            $properties['height'] = (int)$node->getAttribute('height');
+          }
+        }
+      }
+      return $properties;
+    }
   }
 }
---
+++ tests/system/Papaya/Media/File/Info/SVGTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * papaya CMS
+ *
+ * @copyright 2000-2018 by papayaCMS project - All rights reserved.
+ * @link http://www.papaya-cms.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ *  You can redistribute and/or modify this script under the terms of the GNU General Public
+ *  License (GPL) version 2, provided that the copyright and license notes, including these
+ *  lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.
+ */
+
+namespace Papaya\Media\File\Info {
+  require_once __DIR__.'/../../../../../bootstrap.php';
+
+  /**
+   * @covers \Papaya\Media\File\Info\SVG
+   */
+  class SVGTest extends \Papaya\TestCase {
+
+    public function testIsSupportedExtensionExpectingTrue() {
+      $info = new SVG(__DIR__.'/TestData/minimum.svg', 'minimum.svg');
+      $this->assertTrue($info->isSupported([]));
+    }
+
+    public function testIsSupportedMimeTypeExpectingTrue() {
+      $info = new SVG(__DIR__.'/TestData/minimum.ext');
+      $this->assertTrue($info->isSupported(['mimetype' => 'image/svg+xml']));
+    }
+
+    public function testReadUsingXMLReader() {
+      if (!extension_loaded('xmlreader')) {
+        $this->markTestSkipped('XMLReader not available');
+      }
+      $info = new SVG(__DIR__.'/TestData/minimum.svg');
+      $this->assertTrue($info['is_valid']);
+      $this->assertEquals(139, $info['width']);
+      $this->assertEquals(144, $info['height']);
+    }
+
+    public function testReadInvalidUsingXMLReader() {
+      if (!extension_loaded('xmlreader')) {
+        $this->markTestSkipped('XMLReader not available');
+      }
+      $info = new SVG('data://text/plain,');
+      $this->assertFalse($info['is_valid']);
+    }
+
+    public function testReadUsingDOM() {
+      $info = new SVG(__DIR__.'/TestData/minimum.svg');
+      $info->forceDOM = TRUE;
+      $this->assertTrue($info['is_valid']);
+      $this->assertEquals(139, $info['width']);
+      $this->assertEquals(144, $info['height']);
+    }
+
+    public function testReadInvalidUsingDOM() {
+      if (!extension_loaded('xmlreader')) {
+        $this->markTestSkipped('XMLReader not available');
+      }
+      $info = new SVG('data://text/plain,');
+      $this->assertFalse($info['is_valid']);
+    }
+
+  }
+
+}
