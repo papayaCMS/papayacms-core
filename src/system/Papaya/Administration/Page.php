67415d8d563b135324e05ff803c1749d4ef2ee48
Thomas Weinert <thomas@weinert.info>
2019-11-19 11:56:45 +0100

    [TEST] Add Tests For Administration\Page

--- src/system/Papaya/Administration/Page.php
+++ src/system/Papaya/Administration/Page.php
@@ -12,246 +12,246 @@
  *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  *  FOR A PARTICULAR PURPOSE.
  */
-namespace Papaya\Administration;
+namespace Papaya\Administration {
 
-use Papaya\Administration\UI as AdministrationUI;
-use Papaya\Request;
-use Papaya\Template;
-use Papaya\UI\Menu;
-use Papaya\UI\Toolbar;
-
-/**
- * Abstract superclass for an administration page.
- *
- * The administration page has three parts (content, navigation, information). The parts are executed
- * one after another with the same parameters. Changes to the parameters of one part are assigned
- * to the next.
- *
- * Here is an composed toolbar sets for each element.
- *
- * @package Papaya-Library
- * @subpackage Administration
- */
-abstract class Page extends \Papaya\Application\BaseObject {
-  /**
-   * @var string|null
-   */
-  private $_moduleId;
+  use Papaya\Administration\UI as AdministrationUI;
+  use Papaya\Application\BaseObject as ApplicationBaseObject;
+  use Papaya\Request;
+  use Papaya\Template;
+  use Papaya\Template\XSLT as XSLTTemplate;
+  use Papaya\UI\Menu;
+  use Papaya\UI\Toolbar;
 
   /**
-   * @var \Papaya\Administration\UI
-   */
-  private $_ui;
-  /**
-   * @var \Papaya\Template\XSLT
-   */
-  private $_template;
-
-  /**
-   * @var Page\Parts
-   */
-  private $_parts;
-
-  /**
-   * @var Toolbar
-   */
-  private $_toolbar;
-
-  /**
-   * @var string
-   */
-  protected $_parameterGroup = '';
-
-  /**
-   * Create page object and store administration UI for later use
+   * Abstract superclass for an administration page.
    *
-   * @param \Papaya\Administration\UI|\Papaya\Template $ui
-   * @param null|string $moduleId
+   * The administration page has three parts (content, navigation, information). The parts are executed
+   * one after another with the same parameters. Changes to the parameters of one part are assigned
+   * to the next.
+   *
+   * Here is an composed toolbar sets for each element.
+   *
+   * @package Papaya-Library
+   * @subpackage Administration
    */
-  public function __construct($ui, $moduleId = NULL) {
-    if ($ui instanceof Template) {
-      // BC, allow old calls
-      $this->_template = $ui;
-    } elseif ($ui instanceof AdministrationUI) {
-      $this->_ui = $ui;
-    } else {
-      throw new \InvalidArgumentException(
-        sprintf(
-          'Argument should be a "%s" and can be a "%s" for old code.',
-          AdministrationUI::class,
-          Template::class
-        )
-      );
+  abstract class Page extends ApplicationBaseObject {
+    /**
+     * @var string|null
+     */
+    private $_moduleId;
+
+    /**
+     * @var AdministrationUI
+     */
+    private $_ui;
+    /**
+     * @var XSLTTemplate
+     */
+    private $_template;
+
+    /**
+     * @var Page\Parts
+     */
+    private $_parts;
+
+    /**
+     * @var Toolbar
+     */
+    private $_toolbar;
+
+    /**
+     * @var string
+     */
+    protected $_parameterGroup = '';
+
+    /**
+     * Create page object and store administration UI for later use
+     *
+     * @param AdministrationUI|Template $ui
+     * @param null|string $moduleId
+     */
+    public function __construct($ui, $moduleId = NULL) {
+      if ($ui instanceof Template) {
+        // BC, allow old calls
+        $this->_template = $ui;
+      } elseif ($ui instanceof AdministrationUI) {
+        $this->_ui = $ui;
+      } else {
+        throw new \InvalidArgumentException(
+          sprintf(
+            'Argument should be a "%s" and can be a "%s" for old code.',
+            AdministrationUI::class,
+            Template::class
+          )
+        );
+      }
+      $this->_moduleId = $moduleId;
     }
-    $this->_moduleId = $moduleId;
-  }
 
-  /**
-   * @return AdministrationUI|null
-   */
-  public function getUI() {
-    return $this->_ui;
-  }
+    /**
+     * @return AdministrationUI|null
+     */
+    public function getUI() {
+      return $this->_ui;
+    }
 
 
-  /**
-   * @return \Papaya\Template
-   */
-  public function getTemplate() {
-    if ($this->_template instanceof Template) {
+    /**
+     * @return Template
+     */
+    public function getTemplate() {
+      if (NULL === $this->_template && ($ui = $this->getUI())) {
+        $this->_template = $ui->template();
+      }
       return $this->_template;
     }
-    if ($ui = $this->getUI()) {
-      return $ui->template();
+
+    /**
+     * @return null|string
+     */
+    public function getModuleId() {
+      return $this->_moduleId;
     }
-    throw new \LogicException('No template object available');
-  }
 
-  /**
-   * @return null|string
-   */
-  public function getModuleId() {
-    return $this->_moduleId;
-  }
-
-  /**
-   * This method needs to be overloaded to create the content part of the page
-   * If an valid part is returned, it will be used first.
-   *
-   * @return Page\Part|false
-   */
-  protected function createContent() {
-    return FALSE;
-  }
-
-  /**
-   * This method needs to be overloaded to create the navigation part of the page.
-   * If an valid part is returned, it will be used after the content part.
-   *
-   * @return Page\Part|false
-   */
-  protected function createNavigation() {
-    return FALSE;
-  }
-
-  /**
-   * This method needs to be overloaded to create the content part of the page.
-   * If an valid part is returned, it will be used last.
-   *
-   * @return Page\Part|false
-   */
-  protected function createInformation() {
-    return FALSE;
-  }
-
-  /**
-   * Make module executable (like a function)
-   */
-  public function __invoke() {
-    $this->execute();
-  }
-
-  /**
-   * Execute the module and add the xml to the layout object
-   */
-  public function execute() {
-    if (!$this->validateAccess()) {
-      $this->papaya()->messages->displayError('Access forbidden.');
-      return;
+    /**
+     * This method needs to be overloaded to create the content part of the page
+     * If an valid part is returned, it will be used first.
+     *
+     * @return Page\Part|false
+     */
+    protected function createContent() {
+      return FALSE;
     }
-    $parts = $this->parts();
-    $restoreParameters = ('get' === $this->papaya()->request->method) && !empty($this->_parameterGroup);
-    $parametersName = [\get_class($this), 'parameters', $this->_parameterGroup];
-    if ($restoreParameters && $parts->parameters()->isEmpty()) {
-      $value = $this->papaya()->session->getValue($parametersName);
-      $parts->parameters()->merge(\is_array($value) ? $value : []);
-      $this->papaya()->request->setParameters(
-        Request::SOURCE_QUERY,
-        $this->papaya()->request->getParameters(Request::SOURCE_QUERY)->set(
-          $this->_parameterGroup, \is_array($value) ? $value : []
-        )
-      );
+
+    /**
+     * This method needs to be overloaded to create the navigation part of the page.
+     * If an valid part is returned, it will be used after the content part.
+     *
+     * @return Page\Part|false
+     */
+    protected function createNavigation() {
+      return FALSE;
     }
-    foreach ($parts as $name => $part) {
-      if (
-        $part instanceof Page\Part &&
-        ($xml = $part->getXML())
-      ) {
-        $this->getTemplate()->add($xml, $this->parts()->getTarget($name));
+
+    /**
+     * This method needs to be overloaded to create the content part of the page.
+     * If an valid part is returned, it will be used last.
+     *
+     * @return Page\Part|false
+     */
+    protected function createInformation() {
+      return FALSE;
+    }
+
+    /**
+     * Make module executable (like a function)
+     */
+    public function __invoke() {
+      $this->execute();
+    }
+
+    /**
+     * Execute the module and add the xml to the layout object
+     */
+    public function execute() {
+      if (!$this->validateAccess()) {
+        $this->papaya()->messages->displayError('Access forbidden.');
+        return;
       }
-    }
-    if ($restoreParameters) {
-      $this->papaya()->session->setValue($parametersName, $parts->parameters()->toArray());
-    }
-    $this->parts()->toolbar()->toolbar($this->toolbar());
-    $this->getTemplate()->addMenu($this->parts()->toolbar()->getXML());
-  }
-
-  /**
-   * Getter/Setter for the parts list
-   *
-   * @param Page\Parts $parts
-   *
-   * @return Page\Parts
-   */
-  public function parts(Page\Parts $parts = NULL) {
-    if ($parts) {
-      $this->_parts = $parts;
-    } elseif (NULL === $this->_parts) {
-      $this->_parts = new Page\Parts($this);
-      $this->_parts->papaya($this->papaya());
-      if (!empty($this->_parameterGroup)) {
-        $this->_parts->parameterGroup($this->_parameterGroup);
+      $parts = $this->parts();
+      $restoreParameters = ('get' === $this->papaya()->request->getMethod()) && !empty($this->_parameterGroup);
+      $parametersName = [\get_class($this), 'parameters', $this->_parameterGroup];
+      if ($restoreParameters && $parts->parameters()->isEmpty()) {
+        $value = $this->papaya()->session->getValue($parametersName);
+        $parts->parameters()->merge(\is_array($value) ? $value : []);
+        $this->papaya()->request->setParameters(
+          Request::SOURCE_QUERY,
+          $this->papaya()->request->getParameters(Request::SOURCE_QUERY)->set(
+            $this->_parameterGroup, \is_array($value) ? $value : []
+          )
+        );
       }
+      foreach ($parts as $name => $part) {
+        if (
+          $part instanceof Page\Part &&
+          ($xml = $part->getXML())
+        ) {
+          $this->getTemplate()->add($xml, $this->parts()->getTarget($name));
+        }
+      }
+      if ($restoreParameters) {
+        $this->papaya()->session->setValue($parametersName, $parts->parameters()->toArray());
+      }
+      $this->parts()->toolbar()->toolbar($this->toolbar());
+      $this->getTemplate()->addMenu($this->parts()->toolbar()->getXML());
     }
-    return $this->_parts;
-  }
 
-  /**
-   * A method called by the parts list, if an part is needed and not already existing in the list.
-   *
-   * It calls different protected methods that can be overload to create the part. If it returns
-   * FALSE the part is ignored.
-   *
-   * @param string $name
-   *
-   * @return false|Page\Part
-   */
-  public function createPart($name) {
-    switch ($name) {
+    /**
+     * Getter/Setter for the parts list
+     *
+     * @param Page\Parts $parts
+     *
+     * @return Page\Parts
+     */
+    public function parts(Page\Parts $parts = NULL) {
+      if ($parts) {
+        $this->_parts = $parts;
+      } elseif (NULL === $this->_parts) {
+        $this->_parts = new Page\Parts($this);
+        $this->_parts->papaya($this->papaya());
+        if (!empty($this->_parameterGroup)) {
+          $this->_parts->parameterGroup($this->_parameterGroup);
+        }
+      }
+      return $this->_parts;
+    }
+
+    /**
+     * A method called by the parts list, if an part is needed and not already existing in the list.
+     *
+     * It calls different protected methods that can be overload to create the part. If it returns
+     * FALSE the part is ignored.
+     *
+     * @param string $name
+     *
+     * @return false|Page\Part
+     */
+    public function createPart($name) {
+      switch ($name) {
       case Page\Parts::PART_CONTENT :
         return $this->createContent();
       case Page\Parts::PART_NAVIGATION :
         return $this->createNavigation();
       case Page\Parts::PART_INFORMATION :
         return $this->createInformation();
+      }
+      return FALSE;
     }
-    return FALSE;
-  }
 
-  /**
-   * Getter/Setter for the action toolbar. The parts append buttons to sets the sets are
-   * appended to the toolbar.
-   *
-   * @param Toolbar $toolbar
-   *
-   * @return Toolbar
-   */
-  public function toolbar(Toolbar $toolbar = NULL) {
-    if ($toolbar) {
-      $this->_toolbar = $toolbar;
-    } elseif (NULL === $this->_toolbar) {
-      $this->_toolbar = new Menu();
-      $this->_toolbar->papaya($this->papaya());
-      $this->_toolbar->identifier = 'edit';
+    /**
+     * Getter/Setter for the action toolbar. The parts append buttons to sets the sets are
+     * appended to the toolbar.
+     *
+     * @param Toolbar $toolbar
+     *
+     * @return Toolbar
+     */
+    public function toolbar(Toolbar $toolbar = NULL) {
+      if ($toolbar) {
+        $this->_toolbar = $toolbar;
+      } elseif (NULL === $this->_toolbar) {
+        $this->_toolbar = new Menu();
+        $this->_toolbar->papaya($this->papaya());
+        $this->_toolbar->identifier = 'edit';
+      }
+      return $this->_toolbar;
     }
-    return $this->_toolbar;
-  }
 
-  /**
-   * @return bool
-   */
-  public function validateAccess() {
-    return TRUE;
+    /**
+     * @return bool
+     */
+    public function validateAccess() {
+      return TRUE;
+    }
   }
 }
--- tests/system/Papaya/Administration/PageTest.php
+++ tests/system/Papaya/Administration/PageTest.php
@@ -13,108 +13,170 @@
  *  FOR A PARTICULAR PURPOSE.
  */
 
-namespace Papaya\Administration;
+namespace Papaya\Administration {
 
-require_once __DIR__.'/../../../bootstrap.php';
+  use InvalidArgumentException;
+  use Papaya\Administration\UI as AdministrationUI;
+  use Papaya\Message\Manager as MessageManager;
+  use Papaya\Session;
+  use Papaya\Template;
+  use Papaya\TestCase;
+  use Papaya\UI\Toolbar;
 
-class PageTest extends \Papaya\TestCase {
-
-  /**
-   * @covers \Papaya\Administration\Page::__construct
-   */
-  public function testConstructor() {
-    $page = new Page_TestProxy($ui = $this->mockPapaya()->administrationUI());
-    $this->assertSame($ui, $page->getUI());
-  }
+  require_once __DIR__.'/../../../bootstrap.php';
 
   /**
    * @covers \Papaya\Administration\Page
    */
-  public function testPageWithoutParts() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|\Papaya\Administration\UI $ui */
-    $ui =  $this->mockPapaya()->administrationUI();
-    /** @var \PHPUnit_Framework_MockObject_MockObject|\Papaya\Template $template */
-    $template = $ui->template();
-    $template
-      ->expects($this->never())
-      ->method('add');
-    $template
-      ->expects($this->once())
-      ->method('addMenu')
-      ->with('');
-    $page = new Page_TestProxy($ui);
-    $page->papaya($this->mockPapaya()->application());
-    $page->execute();
+  class PageTest extends TestCase {
+
+    public function testConstructor() {
+      $page = new Page_TestProxy($ui = $this->mockPapaya()->administrationUI());
+      $this->assertSame($ui, $page->getUI());
+    }
+
+    public function testConstructorWithModuleId() {
+      $page = new Page_TestProxy($ui = $this->mockPapaya()->administrationUI(), 'abc123');
+      $this->assertSame('abc123', $page->getModuleId());
+    }
+
+    public function testConstructorWithTemplateForBC() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Template $template */
+      $template = $this->createMock(Template::class);
+      $page = new Page_TestProxy($template);
+      $this->assertSame($template, $page->getTemplate());
+    }
+
+    public function testConstructorWithInvalidArgumentExpectingException() {
+      $this->expectException(InvalidArgumentException::class);
+      $this->expectExceptionMessage(
+        'Argument should be a "Papaya\Administration\UI" and can be a "Papaya\Template" for old code.'
+      );
+      /** @noinspection PhpParamsInspection */
+      new Page_TestProxy(new \stdClass());
+    }
+
+    public function testPageExecuteWithoutParts() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|AdministrationUI $ui */
+      $ui = $this->mockPapaya()->administrationUI();
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Template $template */
+      $template = $ui->template();
+      $template
+        ->expects($this->never())
+        ->method('add');
+      $template
+        ->expects($this->once())
+        ->method('addMenu')
+        ->with('');
+      $page = new Page_TestProxy($ui);
+      $page->papaya($this->mockPapaya()->application());
+      $page();
+    }
+
+    public function testPageExecuteWithContentPart() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|AdministrationUI $ui */
+      $ui = $this->mockPapaya()->administrationUI();
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Template $template */
+      $template = $ui->template();
+      $template
+        ->expects($this->once())
+        ->method('add')
+        ->with(
+        /** @lang XML */
+          '<foo/>', 'centercol'
+        );
+      $template
+        ->expects($this->once())
+        ->method('addMenu');
+      $content = $this->createMock(Page\Part::class);
+      $content
+        ->expects($this->once())
+        ->method('getXml')
+        ->willReturn(
+        /** @lang XML */
+          '<foo/>'
+        );
+      $page = new Page_TestProxy($ui);
+      $page->papaya($this->mockPapaya()->application());
+      $page->parts()->content = $content;
+      $page();
+    }
+
+    public function testExecuteWithAccessForbidden() {
+      $messages = $this->createMock(MessageManager::class);
+      $messages
+        ->expects($this->once())
+        ->method('displayError')
+        ->with('Access forbidden.');
+
+      $page = new Page_TestProxy($this->mockPapaya()->administrationUI());
+      $page->papaya($this->mockPapaya()->application(['messages' => $messages]));
+
+      $page->allowAccess = FALSE;
+      $page();
+    }
+
+    public function testExecuteWithParameterGroupKeepsStatusInSession() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Session $session */
+      $session = $this->createMock(Session::class);
+      $session
+        ->expects($this->once())
+        ->method('getValue')
+        ->with([Page_TestProxy::class, 'parameters', 'demo'])
+        ->willReturn(['a-parameter' => 'a-value']);
+      $session
+        ->expects($this->once())
+        ->method('setValue')
+        ->with([Page_TestProxy::class, 'parameters', 'demo'], ['a-parameter' => 'a-value']);
+
+      $page = new Page_TestProxy($this->mockPapaya()->administrationUI());
+      $page->papaya(
+        $this->mockPapaya()->application(['session' => $session])
+      );
+
+      $page->_parameterGroup = 'demo';
+      $page();
+    }
+
+    public function testCreatePartWithUnknownNameExpectingFalse() {
+      $page = new Page_TestProxy($this->mockPapaya()->administrationUI());
+      $this->assertFalse($page->createPart('NonExistingPart'));
+    }
+
+    public function testPartsGetAfterSet() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Page\Parts $parts */
+      $parts = $this
+        ->getMockBuilder(Page\Parts::class)
+        ->disableOriginalConstructor()
+        ->getMock();
+      $page = new Page_TestProxy($this->mockPapaya()->administrationUI());
+      $page->parts($parts);
+      $this->assertSame($parts, $page->parts());
+    }
+
+    public function testToolbarGetAfterSet() {
+      /** @var \PHPUnit_Framework_MockObject_MockObject|Toolbar $toolbar */
+      $toolbar = $this->createMock(Toolbar::class);
+      $page = new Page_TestProxy($this->mockPapaya()->administrationUI());
+      $page->toolbar($toolbar);
+      $this->assertSame($toolbar, $page->toolbar());
+    }
+
+    public function testToolbarGetImplicitCreate() {
+      $page = new Page_TestProxy($this->mockPapaya()->administrationUI());
+      $this->assertInstanceOf(Toolbar::class, $page->toolbar());
+    }
   }
 
-  /**
-   * @covers \Papaya\Administration\Page
-   */
-  public function testPageWithContentPart() {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|\Papaya\Administration\UI $ui */
-    $ui =  $this->mockPapaya()->administrationUI();
-    /** @var \PHPUnit_Framework_MockObject_MockObject|\Papaya\Template $template */
-    $template = $ui->template();
-    $template
-      ->expects($this->once())
-      ->method('add')
-      ->with(
-      /** @lang XML */
-        '<foo/>', 'centercol');
-    $template
-      ->expects($this->once())
-      ->method('addMenu');
-    $content = $this->createMock(Page\Part::class);
-    $content
-      ->expects($this->once())
-      ->method('getXml')
-      ->willReturn(/** @lang XML */
-        '<foo/>');
-    $page = new Page_TestProxy($ui);
-    $page->papaya($this->mockPapaya()->application());
-    $page->parts()->content = $content;
-    $page->execute();
+  class Page_TestProxy extends Page {
+
+    public $allowAccess = TRUE;
+
+    public $_parameterGroup;
+
+    public function validateAccess() {
+      return $this->allowAccess && parent::validateAccess();
+    }
+
   }
-
-  /**
-   * @covers \Papaya\Administration\Page::createPart
-   */
-  public function testCreatePartWithUnknownNameExpectingFalse() {
-    $page = new Page_TestProxy($this->mockPapaya()->administrationUI());
-    $this->assertFalse($page->createPart('NonExistingPart'));
-  }
-
-  /**
-   * @covers \Papaya\Administration\Page::parts
-   */
-  public function testPartsGetAfterSet() {
-    $parts = $this
-      ->getMockBuilder(Page\Parts::class)
-      ->disableOriginalConstructor()
-      ->getMock();
-    $page = new Page_TestProxy($this->mockPapaya()->administrationUI());
-    $page->parts($parts);
-    $this->assertSame($parts, $page->parts());
-  }
-
-  /**
-   * @covers \Papaya\Administration\Page::toolbar
-   */
-  public function testToolbarGetAfterSet() {
-    $page = new Page_TestProxy($this->mockPapaya()->administrationUI());
-    $page->toolbar($toolbar = $this->createMock(\Papaya\UI\Toolbar::class));
-    $this->assertSame($toolbar, $page->toolbar());
-  }
-
-  /**
-   * @covers \Papaya\Administration\Page::toolbar
-   */
-  public function testToolbarGetImplicitCreate() {
-    $page = new Page_TestProxy($this->mockPapaya()->administrationUI());
-    $this->assertInstanceOf(\Papaya\UI\Toolbar::class, $page->toolbar());
-  }
-}
-
-class Page_TestProxy extends Page {
-
 }
