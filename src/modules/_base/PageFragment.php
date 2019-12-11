ce9201573ed6243e0393ad9473b808e56e24e3a7
Thomas Weinert <thomas@weinert.info>
2019-12-11 12:02:43 +0100

    [FEATURE] Add Page Fragment Plugin
    This page module provides a teaser and will redirect to its
    parent page. It allows to use teasers als fragments for an
    aggregation page like Category or CategoryGroup.

---
+++ src/modules/_base/PageFragment.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * papaya CMS
+ *
+ * @copyright 2000-2019 by papayaCMS project - All rights reserved.
+ * @link http://www.papaya-cms.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ *  You can redistribute and/or modify this script under the terms of the GNU General Public
+ *  License (GPL) version 2, provided that the copyright and license notes, including these
+ *  lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.
+ */
+
+namespace Papaya\Modules\Core {
+
+  use Papaya\Plugin\PageModule;
+  use Papaya\Plugin\Routable as RoutablePlugin;
+  use Papaya\Response;
+  use Papaya\Router;
+
+  class PageFragment extends Partials\Teaser implements RoutablePlugin {
+
+    use PageModule\Aggregation;
+
+    /**
+     * Redirect to parent page.
+     *
+     * @param Router $router
+     * @param NULL|object $context
+     * @param int $level
+     * @return Response\Failure|Response\Redirect
+     */
+    public function __invoke(Router $router, $context = NULL, $level = 0) {
+      $reference = $this->papaya()->pageReferences->get(
+        $this->papaya()->request->languageIdentifier,
+        $this->getPage()->getParentID()
+      );
+      return new Response\Redirect((string)$reference);
+    }
+  }
+}
