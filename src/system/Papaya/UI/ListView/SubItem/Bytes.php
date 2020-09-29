aac298fdada24e6112d8cad09127a6105cc0e2e6
Thomas Weinert <thomas@weinert.info>
2020-09-29 18:43:17 +0200

    [REFACTOR] Media Folder/File Navigation

---
+++ src/system/Papaya/UI/ListView/SubItem/Bytes.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * papaya CMS
+ *
+ * @copyright 2000-2020 by papayaCMS project - All rights reserved.
+ * @link http://www.papaya-cms.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ *  You can redistribute and/or modify this script under the terms of the GNU General Public
+ *  License (GPL) version 2, provided that the copyright and license notes, including these
+ *  lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.
+ */
+
+namespace Papaya\UI\ListView\SubItem {
+
+  class Bytes extends Text  {
+
+    /**
+     * @param int $size
+     * @param array|null $actionParameters
+     */
+    public function __construct($size, array $actionParameters = NULL) {
+      parent::__construct(\Papaya\Utility\Bytes::toString($size), $actionParameters);
+    }
+
+  }
+
+}
